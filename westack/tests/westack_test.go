5133805832c78260cc19dd103b842f3a3ca34990
Jhon Magdalena <jfmv.vt@gmail.com>
2021-12-03 13:55:21 +0100

    ADD: test login

--- westack/tests/westack_test.go
+++ westack/tests/westack_test.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"github.com/fredyk/westack-go/westack"
 	"github.com/stretchr/testify/assert"
+	"io"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 )
 
@@ -21,17 +23,62 @@ func init() {
 	})
 }
 
-func Test_WeStack(t *testing.T) {
+func Test_WeStackCreateUser(t *testing.T) {
 
 	m, b := map[string]interface{}{"email": "email1@example.com", "password": "test"}, new(bytes.Buffer)
 	err := json.NewEncoder(b).Encode(m)
 	if err != nil {
 		t.Error(err)
+		return
 	}
 	response, err := app.Server.Test(httptest.NewRequest("POST", "/api/v1/users", b))
 	if err != nil {
 		t.Error(err)
+		return
 	}
-	assert.Equal(t, 200, response.StatusCode)
+	if !assert.Equal(t, 200, response.StatusCode) {
+		return
+	}
+
+}
+
+func Test_WeStackCreateLogin(t *testing.T) {
+
+	m, b := map[string]interface{}{"email": "email1@example.com", "password": "test"}, new(bytes.Buffer)
+	err := json.NewEncoder(b).Encode(m)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	response, err := app.Server.Test(httptest.NewRequest("POST", "/api/v1/users/login", b))
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if !assert.Equal(t, 200, response.StatusCode) {
+		return
+	}
+
+	contentLength, err := strconv.Atoi(response.Header["Content-Length"][0])
+	responseBytes := make([]byte, contentLength)
+	count, err := response.Body.Read(responseBytes)
+	if err != nil && err != io.EOF {
+		t.Error(err)
+		return
+	}
+	if !assert.Greater(t, count, 0) {
+		return
+	}
+
+	var loginResponse map[string]interface{}
+	err = json.Unmarshal(responseBytes, &loginResponse)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if !assert.NotEmpty(t, loginResponse["id"]) {
+		return
+	}
 
 }
