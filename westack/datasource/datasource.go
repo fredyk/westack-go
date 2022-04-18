d52708278d1f12a48d613caa3bea02833cc3fc5f
Jhon Magdalena Vila <6827508+fredyk@users.noreply.github.com>
2022-04-18 13:56:15 +0200

    ADD: Parse non-UTC dates

--- westack/datasource/datasource.go
+++ westack/datasource/datasource.go
@@ -265,10 +265,12 @@ func ReplaceObjectIds(data interface{}) interface{} {
 		var err error
 		if regexp.MustCompile("^([0-9a-f]{24})$").MatchString(data.(string)) {
 			newValue, err = primitive.ObjectIDFromHex(data.(string))
-			//} else if regexp.MustCompile("^(\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(?:\\.\\d+)?)([+:\\-/0-9a-zA-Z]+)?$").MatchString(data.(string)) {
+		} else if regexp.MustCompile("^(\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(?:\\.\\d+)?)([+\\-:0-9]+)$").MatchString(data.(string)) {
+			layout := "2006-01-02T15:04:05.000-0700"
+			newValue, err = time.Parse(layout, regexp.MustCompile("([+\\-]\\d{2}):(\\d{2})$").ReplaceAllString(data.(string), "$1$2"))
+			//log.Printf("DEBUG: Converted date %v to %v\n", data, newValue)
 		} else if regexp.MustCompile("^(\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(?:\\.\\d+)?)([Z]+)?$").MatchString(data.(string)) {
 			//	TODO: parse all type of dates
-			//layout := "2006-01-02T15:04:05.000-03:00"
 			layout := "2006-01-02T15:04:05.000Z"
 			newValue, err = time.Parse(layout, data.(string))
 		}
