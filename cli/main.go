2f6078ec25884e5334d5416431a98b140b6401e4
Jhon Magdalena Vila <6827508+fredyk@users.noreply.github.com>
2022-04-23 13:25:10 +0200

    UPDATE: Remove legacy ACLs and add cli

---
+++ cli/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"github.com/fredyk/westack-go/westack/model"
+	"log"
+	"os"
+)
+
+func main() {
+
+	if len(os.Args) < 2 {
+		printHelp()
+		return
+	}
+
+	cmd := os.Args[1]
+
+	switch cmd {
+	case "init":
+
+		if len(os.Args) < 3 {
+			printHelp()
+			return
+		}
+
+		err := initProject(os.Args[2])
+		if err != nil {
+			log.Fatalln(err)
+		}
+		break
+	case "model":
+		if len(os.Args) < 3 {
+			printHelp()
+			return
+		}
+
+		switch os.Args[2] {
+		case "add":
+			if len(os.Args) < 5 {
+				printHelp()
+				return
+			}
+			err := addModel(model.Config{
+				Name:       os.Args[3],
+				Plural:     "",
+				Base:       "PersistedModel",
+				Public:     true,
+				Properties: map[string]model.Property{},
+				Relations:  &map[string]*model.Relation{},
+				Hidden:     []string{},
+				Casbin:     model.CasbinConfig{},
+			}, os.Args[4])
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}
+
+		break
+	case "help":
+		printHelp()
+		break
+	default:
+		printHelp()
+		break
+	}
+}
