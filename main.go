42ecbaad63fd951beb3d80070f2ff8d470fbe832
Jhon Magdalena Vila <6827508+fredyk@users.noreply.github.com>
2022-04-23 14:10:30 +0200

    ADD: Run from core

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"github.com/fredyk/westack-go/examples/basic-auth-example/server"
+	"github.com/fredyk/westack-go/westack"
+	"log"
+	"os"
+)
 
 func main() {
-	log.Println("You may try github.com/fredyk/westack-go/examples/basic-auth-example")
+	debug := false
+	if envDebug, _ := os.LookupEnv("DEBUG"); envDebug == "true" {
+		debug = true
+	}
+	jwtSecretKey := ""
+	if s, present := os.LookupEnv("JWT_SECRET"); present {
+		jwtSecretKey = s
+		if debug {
+			log.Printf("<JWT_SECRET size=%v> found\n", len(jwtSecretKey))
+		}
+	}
+	app := westack.New(westack.Options{
+		Debug:        debug,
+		RestApiRoot:  "/api/v1",
+		Port:         8023,
+		JwtSecretKey: []byte(jwtSecretKey),
+	})
+
+	app.Boot(server.ServerBoot)
+
+	app.Server.Get("/*", func(c *fiber.Ctx) error {
+		log.Println("GET: " + c.Path())
+		return c.Status(404).JSON(fiber.Map{"error": fiber.Map{"status": 404, "message": fmt.Sprintf("Unknown method %v %v", c.Method(), c.Path())}})
+	})
+	app.Server.Post("/*", func(c *fiber.Ctx) error {
+		log.Println("POST: " + c.Path())
+		return c.Status(404).JSON(fiber.Map{"error": fiber.Map{"status": 404, "message": fmt.Sprintf("Unknown method %v %v", c.Method(), c.Path())}})
+	})
+
+	log.Fatal(app.Start(fmt.Sprintf(":%v", app.Port)))
+
 }
