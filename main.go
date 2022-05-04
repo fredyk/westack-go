28bea3ca3ecfc307868e5258492e075873dd1357
Jhon Magdalena <jhon.magdalena@senseitrade.com>
2022-05-04 13:38:38 +0200

    UPDATE: Simplify app start

--- main.go
+++ main.go
@@ -1,26 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/fredyk/westack-go/westack"
-	"github.com/gofiber/fiber/v2"
-	"log"
 )
 
 func main() {
-	app := westack.New()
-
-	app.Boot()
-
-	app.Server.Get("/*", func(c *fiber.Ctx) error {
-		log.Println("GET: " + c.Path())
-		return c.Status(404).JSON(fiber.Map{"error": fiber.Map{"status": 404, "message": fmt.Sprintf("Unknown method %v %v", c.Method(), c.Path())}})
-	})
-	app.Server.Post("/*", func(c *fiber.Ctx) error {
-		log.Println("POST: " + c.Path())
-		return c.Status(404).JSON(fiber.Map{"error": fiber.Map{"status": 404, "message": fmt.Sprintf("Unknown method %v %v", c.Method(), c.Path())}})
-	})
-
-	log.Fatal(app.Start(fmt.Sprintf(":%v", app.Port)))
-
+	westack.InitAndServe()
 }
