81e065e79f8f24dfc285849c09de884aaca117a9
Jhon Magdalena Vila <6827508+fredyk@users.noreply.github.com>
2022-08-24 00:30:10 +0200

    FIX: missing files

---
+++ westack/model/modelrelations.go
@@ -0,0 +1,447 @@
+package model
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	wst "github.com/fredyk/westack-go/westack/common"
+	"github.com/fredyk/westack-go/westack/datasource"
+)
+
+func isManyRelation(relationType string) bool {
+	return relationType == "hasMany" || relationType == "hasManyThrough" || relationType == "hasAndBelongsToMany"
+}
+
+func isSingleRelation(relationType string) bool {
+	return relationType == "hasOne" || relationType == "belongsTo"
+}
+
+func (loadedModel *Model) ExtractLookupsFromFilter(filterMap *wst.Filter, disableTypeConversions bool) *wst.A {
+
+	if filterMap == nil {
+		return nil
+	}
+
+	var targetWhere *wst.Where
+	if filterMap != nil && filterMap.Where != nil {
+		whereCopy := *filterMap.Where
+		targetWhere = &whereCopy
+	} else {
+		targetWhere = nil
+	}
+
+	var targetOrder *wst.Order
+	if filterMap != nil && filterMap.Order != nil {
+		orderValue := *filterMap.Order
+		targetOrder = &orderValue
+	} else {
+		targetOrder = nil
+	}
+	var targetSkip = filterMap.Skip
+	var targetLimit = filterMap.Limit
+
+	var lookups *wst.A
+	if targetWhere != nil {
+		if !disableTypeConversions {
+			datasource.ReplaceObjectIds(*targetWhere)
+		}
+		lookups = &wst.A{
+			{"$match": *targetWhere},
+		}
+	} else {
+		lookups = &wst.A{}
+	}
+
+	if targetOrder != nil && len(*targetOrder) > 0 {
+		orderMap := wst.M{}
+		for _, orderPair := range *targetOrder {
+			splt := strings.Split(orderPair, " ")
+			key := splt[0]
+			directionSt := splt[1]
+			if strings.ToLower(strings.TrimSpace(directionSt)) == "asc" {
+				orderMap[key] = 1
+			} else if strings.ToLower(strings.TrimSpace(directionSt)) == "desc" {
+				orderMap[key] = -1
+			} else {
+				panic(fmt.Sprintf("Invalid direction %v while trying to sort by %v", directionSt, key))
+			}
+		}
+		*lookups = append(*lookups, wst.M{
+			"$sort": orderMap,
+		})
+	}
+
+	if targetSkip > 0 {
+		*lookups = append(*lookups, wst.M{
+			"$skip": targetSkip,
+		})
+	}
+	if targetLimit > 0 {
+		*lookups = append(*lookups, wst.M{
+			"$limit": targetLimit,
+		})
+	}
+
+	var targetInclude *wst.Include
+	if filterMap != nil && filterMap.Include != nil {
+		includeAsInterfaces := *filterMap.Include
+		targetInclude = &includeAsInterfaces
+	} else {
+		targetInclude = nil
+	}
+	if targetInclude != nil {
+		for _, includeItem := range *targetInclude {
+
+			var targetScope *wst.Filter
+			if includeItem.Scope != nil {
+				scopeValue := *includeItem.Scope
+				targetScope = &scopeValue
+			} else {
+				targetScope = nil
+			}
+
+			relationName := includeItem.Relation
+			relation := (*loadedModel.Config.Relations)[relationName]
+			relatedModelName := relation.Model
+			relatedLoadedModel := (*loadedModel.modelRegistry)[relatedModelName]
+
+			if relatedLoadedModel == nil {
+				log.Println()
+				log.Printf("WARNING: related model %v not found for relation %v.%v", relatedModelName, loadedModel.Name, relationName)
+				log.Println()
+				continue
+			}
+
+			if relatedLoadedModel.Datasource.Name == loadedModel.Datasource.Name {
+				switch relation.Type {
+				case "belongsTo", "hasOne", "hasMany":
+					var matching wst.M
+					var lookupLet wst.M
+					switch relation.Type {
+					case "belongsTo":
+						lookupLet = wst.M{
+							*relation.ForeignKey: fmt.Sprintf("$%v", *relation.ForeignKey),
+						}
+						matching = wst.M{
+							"$eq": []string{fmt.Sprintf("$%v", *relation.PrimaryKey), fmt.Sprintf("$$%v", *relation.ForeignKey)},
+						}
+						break
+					case "hasOne", "hasMany":
+						lookupLet = wst.M{
+							*relation.ForeignKey: fmt.Sprintf("$%v", *relation.PrimaryKey),
+						}
+						matching = wst.M{
+							"$eq": []string{fmt.Sprintf("$%v", *relation.ForeignKey), fmt.Sprintf("$$%v", *relation.ForeignKey)},
+						}
+						break
+					}
+					pipeline := []interface{}{
+						wst.M{
+							"$match": wst.M{
+								"$expr": wst.M{
+									"$and": wst.A{
+										matching,
+									},
+								},
+							},
+						},
+					}
+					project := wst.M{}
+					for _, propertyName := range relatedLoadedModel.Config.Hidden {
+						project[propertyName] = false
+					}
+					if len(project) > 0 {
+						pipeline = append(pipeline, wst.M{
+							"$project": project,
+						})
+					}
+					if targetScope != nil {
+						nestedLoopkups := relatedLoadedModel.ExtractLookupsFromFilter(targetScope, disableTypeConversions)
+						if nestedLoopkups != nil {
+							for _, v := range *nestedLoopkups {
+								pipeline = append(pipeline, v)
+							}
+						}
+					}
+
+					*lookups = append(*lookups, wst.M{
+						"$lookup": wst.M{
+							"from":     relatedLoadedModel.CollectionName,
+							"let":      lookupLet,
+							"pipeline": pipeline,
+							"as":       relationName,
+						},
+					})
+					break
+				}
+				switch relation.Type {
+				case "hasOne", "belongsTo":
+					*lookups = append(*lookups, wst.M{
+						"$unwind": wst.M{
+							"path":                       fmt.Sprintf("$%v", relationName),
+							"preserveNullAndEmptyArrays": true,
+						},
+					})
+					break
+				}
+
+			}
+		}
+
+	} else {
+
+	}
+	return lookups
+}
+
+/*
+params:
+	- relationDeepLevel: Starts at 1 (Root is 0)
+*/
+func (loadedModel *Model) mergeRelated(relationDeepLevel byte, documents *wst.A, includeItem wst.IncludeItem, baseContext *EventContext) error {
+
+	if documents == nil {
+		return nil
+	}
+
+	parentDocs := documents
+
+	relationName := includeItem.Relation
+	relation := (*loadedModel.Config.Relations)[relationName]
+	relatedModelName := relation.Model
+	relatedLoadedModel := (*loadedModel.modelRegistry)[relatedModelName]
+
+	parentModel := loadedModel
+	parentRelationName := relationName
+
+	if relatedLoadedModel == nil {
+		log.Println()
+		log.Printf("WARNING: related model %v not found for relation %v.%v", relatedModelName, loadedModel.Name, relationName)
+		log.Println()
+		return nil
+	}
+
+	//if relation.Options.SkipAuth && relationDeepLevel > 1 {
+	// Only skip auth checking for relations above the level 1
+	if relation.Options.SkipAuth {
+		if loadedModel.App.Debug {
+			log.Printf("DEBUG: SkipAuth %v.%v\n", loadedModel.Name, relationName)
+		}
+	} else {
+		objId := "*"
+		if len(*documents) == 1 {
+			objId = (*documents)[0]["_id"].(primitive.ObjectID).Hex()
+		}
+
+		action := fmt.Sprintf("__get__%v", relationName)
+		if loadedModel.App.Debug {
+			log.Printf("DEBUG: Check %v.%v\n", loadedModel.Name, action)
+		}
+		err, allowed := loadedModel.EnforceEx(baseContext.Bearer, objId, action, baseContext)
+		if err != nil && err != fiber.ErrUnauthorized {
+			return err
+		}
+		if !allowed {
+			for _, doc := range *documents {
+				delete(doc, relationName)
+			}
+		}
+	}
+
+	if relatedLoadedModel.Datasource.Name != loadedModel.Datasource.Name {
+		switch relation.Type {
+		case "belongsTo", "hasOne", "hasMany":
+			keyFrom := ""
+			keyTo := ""
+			switch relation.Type {
+			case "belongsTo":
+				keyFrom = *relation.PrimaryKey
+				keyTo = *relation.ForeignKey
+				break
+			case "hasOne", "hasMany":
+				keyFrom = *relation.ForeignKey
+				keyTo = *relation.PrimaryKey
+				break
+			}
+
+			var targetScope *wst.Filter
+			if includeItem.Scope != nil {
+				scopeValue := *includeItem.Scope
+				targetScope = &scopeValue
+			} else {
+				targetScope = &wst.Filter{}
+			}
+
+			wasEmptyWhere := false
+			if targetScope.Where == nil {
+				targetScope.Where = &wst.Where{}
+				wasEmptyWhere = true
+			}
+
+			cachedRelatedDocs := make([]InstanceA, len(*documents))
+			localCache := map[string]InstanceA{}
+
+			for documentIdx, document := range *documents {
+
+				if wasEmptyWhere && relatedLoadedModel.Config.Cache.Datasource != "" /* && keyFrom == relatedLoadedModel.Config.Cache.Keys*/ {
+
+					cacheDs, err := loadedModel.App.FindDatasource(relatedLoadedModel.Config.Cache.Datasource)
+					if err != nil {
+						return err
+					}
+					safeCacheDs := cacheDs.(*datasource.Datasource)
+
+					//baseKey := fmt.Sprintf("%v:%v", safeCacheDs.Viper.GetString(safeCacheDs.Key+".database"), relatedLoadedModel.Config.Name)
+					for _, keyGroup := range relatedLoadedModel.Config.Cache.Keys {
+
+						if len(keyGroup) == 1 && keyGroup[0] == keyFrom {
+
+							cacheKeyTo := fmt.Sprintf("%v:%v", keyFrom, document[keyTo])
+
+							if localCache[cacheKeyTo] != nil {
+								cachedRelatedDocs[documentIdx] = localCache[cacheKeyTo]
+							} else {
+								var cachedDocs *wst.A
+
+								cacheLookups := &wst.A{wst.M{"$match": wst.M{keyFrom: cacheKeyTo}}}
+								cachedDocs, err = safeCacheDs.FindMany(relatedLoadedModel.CollectionName, cacheLookups)
+								if err != nil {
+									return err
+								}
+
+								for _, cachedDoc := range *cachedDocs {
+									cachedInstance := relatedLoadedModel.Build(cachedDoc, baseContext)
+									if cachedRelatedDocs[documentIdx] == nil {
+										cachedRelatedDocs[documentIdx] = InstanceA{}
+									}
+									cachedRelatedDocs[documentIdx] = append(cachedRelatedDocs[documentIdx], cachedInstance)
+								}
+								localCache[cacheKeyTo] = cachedRelatedDocs[documentIdx]
+							}
+
+						}
+					}
+
+				}
+
+				relatedInstances := cachedRelatedDocs[documentIdx]
+				if relatedInstances == nil {
+
+					(*targetScope.Where)[keyFrom] = document[keyTo]
+					if isSingleRelation(relation.Type) {
+						targetScope.Limit = 1
+					}
+
+					var err error
+					relatedInstances, err = relatedLoadedModel.FindMany(targetScope, baseContext)
+					if err != nil {
+						return err
+					}
+				} else {
+					if loadedModel.App.Debug {
+						log.Printf("Found cache for %v.%v[%v]\n", loadedModel.Name, relationName, documentIdx)
+					}
+				}
+
+				if loadedModel.hasHiddenProperties {
+					for _, relatedInstance := range relatedInstances {
+						relatedInstance.HideProperties()
+					}
+				}
+
+				switch {
+				case isSingleRelation(relation.Type):
+					if len(relatedInstances) > 0 {
+						document[relationName] = relatedInstances[0]
+					} else {
+						document[relationName] = nil
+					}
+					break
+				case isManyRelation(relation.Type):
+					document[relationName] = relatedInstances
+					break
+				}
+
+			}
+
+			break
+		}
+
+	} else {
+
+		if includeItem.Scope != nil && documents != nil && len(*documents) > 0 {
+			if includeItem.Scope.Include != nil {
+
+				for _, includeItem := range *includeItem.Scope.Include {
+					relationName := includeItem.Relation
+					//relation := (*loadedModel.Config.Relations)[relationName]
+					_isSingleRelation := isSingleRelation(relation.Type)
+					_isManyRelation := !_isSingleRelation
+					//relatedModelName := relation.Model
+					//relatedLoadedModel := (*loadedModel.modelRegistry)[relatedModelName]
+
+					nestedDocuments := make(wst.A, 0)
+
+					for _, doc := range *documents {
+
+						switch {
+						case _isSingleRelation:
+							if doc[parentRelationName] != nil {
+								documentsValue := make(wst.A, 1)
+
+								if relatedInstance, ok := doc[parentRelationName].(map[string]interface{}); ok {
+									documentsValue[0] = wst.M{}
+									for k, v := range relatedInstance {
+										documentsValue[0][k] = v
+									}
+								} else if relatedInstance, ok := doc[parentRelationName].(wst.M); ok {
+									documentsValue[0] = relatedInstance
+								} else {
+									log.Printf("WARNING: Invalid type for %v.%v %s\n", loadedModel.Name, relationName, doc[parentRelationName])
+								}
+
+								//documents = &documentsValue
+								nestedDocuments = append(nestedDocuments, documentsValue...)
+							}
+							break
+						case _isManyRelation:
+							if doc[parentRelationName] != nil {
+
+								if asGeneric, ok := doc[parentRelationName].([]interface{}); ok {
+									relatedInstances := asGeneric
+									nestedDocuments = append(nestedDocuments, *wst.AFromGenericSlice(&relatedInstances)...)
+								} else if asPrimitiveA, ok := doc[parentRelationName].(primitive.A); ok {
+									relatedInstances := asPrimitiveA
+									nestedDocuments = append(nestedDocuments, *wst.AFromPrimitiveSlice(&relatedInstances)...)
+								} else if asA, ok := doc[parentRelationName].(wst.A); ok {
+									nestedDocuments = append(nestedDocuments, asA...)
+								} else {
+									log.Println("WARNING: unknown type for relation", relationName, "in", loadedModel.Name)
+									continue
+								}
+
+							}
+							break
+						}
+
+					}
+					loadedModel := relatedLoadedModel
+					if loadedModel.App.Debug {
+						log.Printf("Dispatch nested relation %v.%v.%v (n=%v, m=%v)\n", parentModel.Name, parentRelationName, relationName, len(*parentDocs), len(nestedDocuments))
+					}
+					err := loadedModel.mergeRelated(relationDeepLevel+1, &nestedDocuments, includeItem, baseContext)
+					if err != nil {
+						return err
+					}
+
+				}
+			}
+		}
+	}
+
+	return nil
+}
---
+++ westack/model/modelremote.go
@@ -0,0 +1,377 @@
+package model
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+
+	wst "github.com/fredyk/westack-go/westack/common"
+	"github.com/fredyk/westack-go/westack/datasource"
+)
+
+func (loadedModel *Model) SendError(ctx *fiber.Ctx, err error) error {
+	switch err.(type) {
+	case *wst.WeStackError:
+		errorName := err.(*wst.WeStackError).Name
+		if errorName == "" {
+			errorName = "Error"
+		}
+		return ctx.Status((err).(*wst.WeStackError).FiberError.Code).JSON(fiber.Map{
+			"error": fiber.Map{
+				"statusCode": (err).(*wst.WeStackError).FiberError.Code,
+				"name":       errorName,
+				"code":       err.(*wst.WeStackError).Code,
+				"error":      err.(*wst.WeStackError).FiberError.Error(),
+				"message":    (err.(*wst.WeStackError).Details)["message"],
+				"details":    err.(*wst.WeStackError).Details,
+			},
+		})
+	default:
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": fiber.Map{
+				"statusCode": 500,
+				"name":       "InternalServerError",
+				"code":       "INTERNAL_SERVER_ERROR",
+				"error":      err.Error(),
+				"message":    err.Error(),
+			},
+		})
+	}
+}
+
+func (loadedModel *Model) RemoteMethod(handler func(context *EventContext) error, options RemoteMethodOptions) fiber.Router {
+	if !loadedModel.Config.Public {
+		panic(fmt.Sprintf("Trying to register a remote method in the private model: %v, you may set \"public\": true in the %v.json file", loadedModel.Name, loadedModel.Name))
+	}
+	options.Name = strings.TrimSpace(options.Name)
+	if options.Name == "" {
+		panic("Method name cannot be empty")
+	}
+	if loadedModel.remoteMethodsMap[options.Name] != nil {
+		panic(fmt.Sprintf("Already registered a remote method with name '%v'", options.Name))
+	}
+
+	var http = options.Http
+	path := http.Path
+	verb := strings.ToLower(http.Verb)
+	description := options.Description
+
+	for _, arg := range options.Accepts {
+		arg.Arg = strings.TrimSpace(arg.Arg)
+		if arg.Arg == "" {
+			panic(fmt.Sprintf("Argument name cannot be empty in the remote method '%v'", options.Name))
+		}
+		if arg.Http.Source != "query" && arg.Http.Source != "body" {
+			panic(fmt.Sprintf("Argument '%v' in the remote method '%v' has an invalid 'in' value: '%v'", arg.Arg, options.Name, arg.Http.Source))
+		}
+	}
+
+	_, err := loadedModel.Enforcer.AddRoleForUser(options.Name, "*")
+	if err != nil {
+		panic(err)
+	}
+
+	var toInvoke func(string, ...fiber.Handler) fiber.Router
+	operation := ""
+
+	switch verb {
+	case "get":
+		toInvoke = (*loadedModel.Router).Get
+		operation = "Finds"
+	case "options":
+		toInvoke = (*loadedModel.Router).Options
+		operation = "Gets options for"
+	case "head":
+		toInvoke = (*loadedModel.Router).Head
+		operation = "Checks"
+	case "post":
+		toInvoke = (*loadedModel.Router).Post
+		operation = "Creates"
+	case "put":
+		toInvoke = (*loadedModel.Router).Put
+		operation = "Replaces"
+	case "patch":
+		toInvoke = (*loadedModel.Router).Patch
+		operation = "Updates attributes in"
+	case "delete":
+		toInvoke = (*loadedModel.Router).Delete
+		operation = "Deletes"
+	}
+
+	fullPath := loadedModel.BaseUrl + "/" + path
+	fullPath = regexp.MustCompile("//+").ReplaceAllString(fullPath, "/")
+	fullPath = regexp.MustCompile(`:(\w+)`).ReplaceAllString(fullPath, "{$1}")
+
+	if (*loadedModel.App.SwaggerPaths())[fullPath] == nil {
+		(*loadedModel.App.SwaggerPaths())[fullPath] = wst.M{}
+	}
+
+	if description == "" {
+		description = fmt.Sprintf("%v %v.", operation, loadedModel.Config.Plural)
+	}
+
+	pathDef := wst.M{
+		//"description": description,
+		//"consumes": []string{
+		//	"*/*",
+		//},
+		//"produces": []string{
+		//	"application/json",
+		//},
+		"tags": []string{
+			loadedModel.Name,
+		},
+		//"requestBody": requestBody,
+		"summary": description,
+		"security": []fiber.Map{
+			{"bearerAuth": []string{}},
+		},
+		"responses": wst.M{
+			"200": wst.M{
+				"description": "OK",
+				"content": wst.M{
+					"application/json": wst.M{
+						"schema": wst.M{
+							"type": "object",
+						},
+					},
+				},
+				//"$ref": "#/components/schemas/" + loadedModel.Config.Name,
+				//"schema": wst.M{
+				//	"type":                 "object",
+				//	"additionalProperties": true,
+				//},
+			},
+		},
+	}
+
+	pathParams := regexp.MustCompile(`:(\w+)`).FindAllString(path, -1)
+
+	params := make([]wst.M, len(pathParams))
+
+	for idx, param := range pathParams {
+		params[idx] = wst.M{
+			"name":     strings.TrimPrefix(param, ":"),
+			"in":       "path",
+			"required": true,
+			"schema": wst.M{
+				"type": "string",
+			},
+		}
+	}
+
+	(*loadedModel.App.SwaggerPaths())[fullPath][verb] = pathDef
+
+	if verb == "post" || verb == "put" || verb == "patch" {
+		pathDef["requestBody"] = wst.M{
+			"description": "data",
+			"required":    true,
+			//"name":        "data",
+			//"in":          "body",
+			//"schema": wst.M{
+			//	"type": "object",
+			//},
+			"content": wst.M{
+				"application/json": wst.M{
+					"schema": wst.M{
+						"type": "object",
+					},
+				},
+			},
+		}
+	} else {
+
+		for _, param := range options.Accepts {
+			paramType := param.Type
+			if paramType == "" {
+				panic(fmt.Sprintf("Argument '%v' in the remote method '%v' has an invalid 'type' value: '%v'", param.Arg, options.Name, paramType))
+			}
+			paramDescription := param.Description
+			if paramType == "date" {
+				paramType = "string"
+				paramDescription += " (format: ISO8601)"
+			}
+			params = append(params, wst.M{
+				"name":        param.Arg,
+				"in":          param.Http.Source,
+				"description": paramDescription,
+				"required":    param.Required,
+				"schema": wst.M{
+					"type": paramType,
+				},
+			})
+		}
+
+	}
+
+	if len(params) > 0 {
+		pathDef["parameters"] = params
+	}
+
+	(*loadedModel.App.SwaggerPaths())[fullPath][verb] = pathDef
+
+	loadedModel.remoteMethodsMap[options.Name] = &OperationItem{
+		Handler: handler,
+		Options: options,
+	}
+
+	return toInvoke(path, func(ctx *fiber.Ctx) error {
+		eventContext := &EventContext{
+			Ctx:    ctx,
+			Remote: &options,
+		}
+		err2 := loadedModel.HandleRemoteMethod(options.Name, eventContext)
+		if err2 != nil {
+
+			if err2 == fiber.ErrUnauthorized {
+				err2 = wst.CreateError(fiber.ErrUnauthorized, "UNAUTHORIZED", fiber.Map{"message": "Unauthorized"}, "Error")
+			}
+
+			log.Printf("Error in remote method %v.%v (%v %v%v): %v\n", loadedModel.Name, options.Name, strings.ToUpper(verb), loadedModel.BaseUrl, path, err2.Error())
+			return loadedModel.SendError(eventContext.Ctx, err2)
+		}
+		return nil
+	})
+}
+
+func (loadedModel *Model) HandleRemoteMethod(name string, eventContext *EventContext) error {
+
+	operationItem := loadedModel.remoteMethodsMap[name]
+
+	if operationItem == nil {
+		return errors.New(fmt.Sprintf("Method '%v' not found", name))
+	}
+
+	c := eventContext.Ctx
+	options := operationItem.Options
+	handler := operationItem.Handler
+
+	err, token := eventContext.GetBearer(loadedModel)
+	if err != nil {
+		return err
+	}
+
+	action := options.Name
+
+	if loadedModel.App.Debug {
+		log.Println(fmt.Sprintf("DEBUG: Check auth for %v.%v (%v %v)", loadedModel.Name, options.Name, c.Method(), c.Path()))
+	}
+
+	objId := "*"
+	if eventContext.ModelID != nil {
+		objId = GetIDAsString(eventContext.ModelID)
+	} else {
+		objId = c.Params("id")
+		if objId == "" {
+			objId = "*"
+		}
+	}
+
+	err, allowed := loadedModel.EnforceEx(token, objId, action, eventContext)
+	if err != nil {
+		return err
+	}
+	if !allowed {
+		return fiber.ErrUnauthorized
+	}
+
+	eventContext.Bearer = token
+
+	eventContext.Data = &wst.M{}
+	eventContext.Query = &wst.M{}
+
+	if strings.ToLower(options.Http.Verb) == "post" || strings.ToLower(options.Http.Verb) == "put" || strings.ToLower(options.Http.Verb) == "patch" {
+		var data *wst.M
+		bytes := eventContext.Ctx.Body()
+		if len(bytes) > 0 {
+			err := json.Unmarshal(bytes, &data)
+			if err != nil {
+				return wst.CreateError(fiber.ErrBadRequest, "INVALID_BODY", fiber.Map{"message": err.Error()}, "ValidationError")
+			}
+			eventContext.Data = data
+		} else {
+			// Empty body is allowed
+		}
+	}
+
+	foundSomeQuery := false
+	for _, paramDef := range options.Accepts {
+		key := paramDef.Arg
+		if paramDef.Http.Source == "body" {
+
+			// Already parsed. Only used for OpenAPI Description
+
+		} else if paramDef.Http.Source == "query" {
+
+			var param interface{}
+			paramSt := c.Query(key, "")
+			switch paramDef.Type {
+			case "string":
+				param = paramSt
+				break
+			case "date":
+				param, err = wst.ParseDate(paramSt)
+				if err != nil {
+					return wst.CreateError(fiber.ErrBadRequest, "INVALID_DATE", fiber.Map{"message": err.Error()}, "ValidationError")
+				}
+				break
+			case "number":
+				param, err = strconv.ParseFloat(paramSt, 64)
+				if err != nil {
+					return wst.CreateError(fiber.ErrBadRequest, "INVALID_NUMBER", fiber.Map{"message": err.Error()}, "ValidationError")
+				}
+				break
+			}
+			(*eventContext.Query)[key] = param
+
+			if paramDef.Arg == "filter" {
+				filterSt := (*eventContext.Query)[key].(string)
+				filterMap := ParseFilter(filterSt)
+
+				eventContext.Filter = filterMap
+				continue
+			}
+
+			foundSomeQuery = true
+
+		}
+	}
+	eventContext.Data = datasource.ReplaceObjectIds(eventContext.Data).(*wst.M)
+	if foundSomeQuery {
+		eventContext.Query = datasource.ReplaceObjectIds(eventContext.Query).(*wst.M)
+	}
+
+	err = handler(eventContext)
+	if err != nil {
+		return err
+	}
+	if eventContext.Result != nil || eventContext.StatusCode != 0 {
+		if eventContext.StatusCode == 0 {
+			eventContext.StatusCode = fiber.StatusOK
+		} else if eventContext.StatusCode == fiber.StatusNoContent {
+			return eventContext.Ctx.Status(fiber.StatusNoContent).SendString("")
+		}
+		switch eventContext.Result.(type) {
+		case wst.M, *wst.M, map[string]interface{}, *map[string]interface{}, wst.A, *wst.A, []interface{}, *[]interface{}, int, int32, int64, float32, float64, bool:
+			return eventContext.Ctx.Status(eventContext.StatusCode).JSON(eventContext.Result)
+		case string:
+			return eventContext.Ctx.Status(eventContext.StatusCode).SendString(eventContext.Result.(string))
+		case []byte:
+			return eventContext.Ctx.Status(eventContext.StatusCode).Send(eventContext.Result.([]byte))
+		default:
+			fmt.Printf("Unknown type: %T after remote method %v\n", eventContext.Result, name)
+		}
+	}
+	resp := eventContext.Ctx.Response()
+	if resp.StatusCode() == 0 {
+		fmt.Printf("WARNING: No result found after remote method %v\n", name)
+		return eventContext.Ctx.Status(fiber.StatusNoContent).Type("application/json").Send([]byte("null"))
+	}
+	return nil
+}
