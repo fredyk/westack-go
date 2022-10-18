70dccf7dca8e39cba8c68f363e8a6ee35e889d26
Jhon Magdalena <jfredy.magdalena@gmail.com>
2022-10-18 14:48:45 +0200

    FIX: relative date placeholders with FindStringSubmatch

--- westack/datasource/datasource.go
+++ westack/datasource/datasource.go
@@ -493,85 +493,85 @@ func ReplaceObjectIds(data interface{}) interface{} {
 			} else {
 				switch value.(type) {
 				case string:
-					if r := regexp.MustCompile(`^\$(\d+)dago$`).FindAllString(value.(string), -1); len(r) > 0 {
+					if r := regexp.MustCompile(`^\$(\d+)dago$`).FindStringSubmatch(value.(string)); len(r) > 1 {
 						atoi, err := strconv.Atoi(r[1])
 						if err != nil {
 							panic(err)
 						}
 						newValue = time.Now().Add(time.Duration(atoi) * 24 * time.Hour)
-					} else if r := regexp.MustCompile(`^\$(\d+)wago$`).FindAllString(value.(string), -1); len(r) > 0 {
+					} else if r := regexp.MustCompile(`^\$(\d+)wago$`).FindStringSubmatch(value.(string)); len(r) > 1 {
 						atoi, err := strconv.Atoi(r[1])
 						if err != nil {
 							panic(err)
 						}
 						newValue = time.Now().Add(time.Duration(atoi) * 7 * 24 * time.Hour)
-					} else if r := regexp.MustCompile(`^\$(\d+)mago$`).FindAllString(value.(string), -1); len(r) > 0 {
+					} else if r := regexp.MustCompile(`^\$(\d+)mago$`).FindStringSubmatch(value.(string)); len(r) > 1 {
 						atoi, err := strconv.Atoi(r[1])
 						if err != nil {
 							panic(err)
 						}
 						newValue = time.Now().Add(time.Duration(atoi) * 30 * 24 * time.Hour)
-					} else if r := regexp.MustCompile(`^\$(\d+)yago$`).FindAllString(value.(string), -1); len(r) > 0 {
+					} else if r := regexp.MustCompile(`^\$(\d+)yago$`).FindStringSubmatch(value.(string)); len(r) > 1 {
 						atoi, err := strconv.Atoi(r[1])
 						if err != nil {
 							panic(err)
 						}
 						newValue = time.Now().Add(time.Duration(atoi) * 365 * 24 * time.Hour)
-					} else if r := regexp.MustCompile(`^\$(\d+)Sago$`).FindAllString(value.(string), -1); len(r) > 0 {
+					} else if r := regexp.MustCompile(`^\$(\d+)Sago$`).FindStringSubmatch(value.(string)); len(r) > 1 {
 						atoi, err := strconv.Atoi(r[1])
 						if err != nil {
 							panic(err)
 						}
 						newValue = time.Now().Add(time.Duration(atoi) * time.Second)
-					} else if r := regexp.MustCompile(`^\$(\d+)Mago$`).FindAllString(value.(string), -1); len(r) > 0 {
+					} else if r := regexp.MustCompile(`^\$(\d+)Mago$`).FindStringSubmatch(value.(string)); len(r) > 1 {
 						atoi, err := strconv.Atoi(r[1])
 						if err != nil {
 							panic(err)
 						}
 						newValue = time.Now().Add(time.Duration(atoi) * time.Minute)
-					} else if r := regexp.MustCompile(`^\$(\d+)Hago$`).FindAllString(value.(string), -1); len(r) > 0 {
+					} else if r := regexp.MustCompile(`^\$(\d+)Hago$`).FindStringSubmatch(value.(string)); len(r) > 1 {
 						atoi, err := strconv.Atoi(r[1])
 						if err != nil {
 							panic(err)
 						}
 						newValue = time.Now().Add(time.Duration(atoi) * time.Hour)
-					} else if r := regexp.MustCompile(`^\$(\d+)dfromnow$`).FindAllString(value.(string), -1); len(r) > 0 {
+					} else if r := regexp.MustCompile(`^\$(\d+)dfromnow$`).FindStringSubmatch(value.(string)); len(r) > 1 {
 						atoi, err := strconv.Atoi(r[1])
 						if err != nil {
 							panic(err)
 						}
 						newValue = time.Now().Add(time.Duration(atoi) * 24 * time.Hour)
-					} else if r := regexp.MustCompile(`^\$(\d+)wfromnow$`).FindAllString(value.(string), -1); len(r) > 0 {
+					} else if r := regexp.MustCompile(`^\$(\d+)wfromnow$`).FindStringSubmatch(value.(string)); len(r) > 1 {
 						atoi, err := strconv.Atoi(r[1])
 						if err != nil {
 							panic(err)
 						}
 						newValue = time.Now().Add(time.Duration(atoi) * 7 * 24 * time.Hour)
-					} else if r := regexp.MustCompile(`^\$(\d+)mfromnow$`).FindAllString(value.(string), -1); len(r) > 0 {
+					} else if r := regexp.MustCompile(`^\$(\d+)mfromnow$`).FindStringSubmatch(value.(string)); len(r) > 1 {
 						atoi, err := strconv.Atoi(r[1])
 						if err != nil {
 							panic(err)
 						}
 						newValue = time.Now().Add(time.Duration(atoi) * 30 * 24 * time.Hour)
-					} else if r := regexp.MustCompile(`^\$(\d+)yfromnow$`).FindAllString(value.(string), -1); len(r) > 0 {
+					} else if r := regexp.MustCompile(`^\$(\d+)yfromnow$`).FindStringSubmatch(value.(string)); len(r) > 1 {
 						atoi, err := strconv.Atoi(r[1])
 						if err != nil {
 							panic(err)
 						}
 						newValue = time.Now().Add(time.Duration(atoi) * 365 * 24 * time.Hour)
-					} else if r := regexp.MustCompile(`^\$(\d+)Sfromnow$`).FindAllString(value.(string), -1); len(r) > 0 {
+					} else if r := regexp.MustCompile(`^\$(\d+)Sfromnow$`).FindStringSubmatch(value.(string)); len(r) > 1 {
 						atoi, err := strconv.Atoi(r[1])
 						if err != nil {
 							panic(err)
 						}
 						newValue = time.Now().Add(time.Duration(atoi) * time.Second)
-					} else if r := regexp.MustCompile(`^\$(\d+)Mfromnow$`).FindAllString(value.(string), -1); len(r) > 0 {
+					} else if r := regexp.MustCompile(`^\$(\d+)Mfromnow$`).FindStringSubmatch(value.(string)); len(r) > 1 {
 						atoi, err := strconv.Atoi(r[1])
 						if err != nil {
 							panic(err)
 						}
 						newValue = time.Now().Add(time.Duration(atoi) * time.Minute)
-					} else if r := regexp.MustCompile(`^\$(\d+)Hfromnow$`).FindAllString(value.(string), -1); len(r) > 0 {
+					} else if r := regexp.MustCompile(`^\$(\d+)Hfromnow$`).FindStringSubmatch(value.(string)); len(r) > 1 {
 						atoi, err := strconv.Atoi(r[1])
 						if err != nil {
 							panic(err)
