199621b6e35287e388cc6809bccd903f80d707f4
Jhon Magdalena <jfredy.magdalena@gmail.com>
2023-02-25 16:02:55 +0100

    ADD: 100% test coverage in common.go

--- westack/tests/westack_common_test.go
+++ westack/tests/westack_common_test.go
@@ -1,13 +1,167 @@
 package tests
 
 import (
+	"os"
 	"testing"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	wst "github.com/fredyk/westack-go/westack/common"
 )
+
+func Test_GetM(t *testing.T) {
+	m := wst.M{"a": wst.M{"b": "z"}}
+	assert.Equal(t, "z", m.GetM("a").GetString("b"))
+}
+
+func Test_GetM_WithNativeMap(t *testing.T) {
+	m := wst.M{"a": map[string]interface{}{"b": "z"}}
+	assert.Equal(t, "z", m.GetM("a").GetString("b"))
+}
+
+func Test_GetM_NonexistentKey(t *testing.T) {
+	m := wst.M{"a": wst.M{"b": "z"}}
+	assert.Nil(t, m.GetM("c"))
+}
+
+func Test_GetM_NilInput(t *testing.T) {
+	var m wst.M = nil
+	assert.Nil(t, m.GetM("a"))
+}
+
+func Test_GetString_NilMap(t *testing.T) {
+	var m wst.M = nil
+	assert.Equal(t, "", m.GetString("a"))
+}
+
+func Test_GetString_NonexistentKey(t *testing.T) {
+	m := wst.M{"a": wst.M{"b": "z"}}
+	assert.Equal(t, "", m.GetString("c"))
+}
+
+func Test_Error(t *testing.T) {
+	err := wst.WeStackError{
+		FiberError: fiber.ErrUnauthorized,
+		Code:       "UNAUTHORIZED",
+		Details:    fiber.Map{"message": "Unauthorized"},
+		Name:       "Error",
+	}
+	// test err.Error()
+	assert.Equal(t, "401 Unauthorized: {\"message\":\"Unauthorized\"}", err.Error())
+}
+
+func Test_Error_WithErorOnMarshalDetails(t *testing.T) {
+	err := wst.WeStackError{
+		FiberError: fiber.ErrUnauthorized,
+		Code:       "UNAUTHORIZED",
+		Details:    fiber.Map{"message": make(chan int)},
+		Name:       "Error",
+	}
+	// test err.Error()
+	assert.Regexpf(t, "401 Unauthorized: map\\[message:0x[0-9a-f]+\\]", err.Error(), "Error message should contain the address of the channel")
+}
+
+func Test_LoadFile_InvalidPath(t *testing.T) {
+	var m wst.M
+	err := wst.LoadFile("invalid_path", &m)
+	assert.Error(t, err)
+}
+
+func Test_LoadFile_InvalidJson(t *testing.T) {
+	var m wst.M
+	// write the json:
+	err := os.WriteFile("invalid.json", []byte("invalid json"), 0644)
+	if err != nil {
+		t.Errorf("Error writing file: %v", err)
+		return
+	}
+	// clean up:
+	defer func() {
+		err := os.Remove("invalid.json")
+		if err != nil {
+			t.Errorf("Error deleting file: %v", err)
+		}
+	}()
+	err = wst.LoadFile("invalid.json", &m)
+
+	assert.Error(t, err)
+}
+
+func Test_DashedCase(t *testing.T) {
+	assert.Equal(t, "hello-world", wst.DashedCase("helloWorld"))
+}
+
+func Test_Transform(t *testing.T) {
+	type OutputType struct {
+		Foo string `json:"foo"`
+	}
+	inputMap := wst.M{"foo": "bar"}
+	var output OutputType
+	err := wst.Transform(inputMap, &output)
+	assert.NoError(t, err)
+	assert.Equal(t, "bar", output.Foo)
+}
+
+func Test_Transform_WithOutputError(t *testing.T) {
+	type OutputType struct {
+		Foo chan int `json:"foo"`
+	}
+	inputMap := wst.M{"foo": "bar"}
+	var output OutputType
+	err := wst.Transform(inputMap, &output)
+	assert.Error(t, err)
+}
+
+func Test_Transform_WithInputError(t *testing.T) {
+	type OutputType struct {
+		Foo string `json:"foo"`
+	}
+	inputMap := wst.M{"foo": make(chan int)}
+	var output OutputType
+	err := wst.Transform(inputMap, &output)
+	assert.Error(t, err)
+}
+
+func Test_ParseDate1(t *testing.T) {
+	d, err := wst.ParseDate("2021-01-01T00:00:00-0500")
+	assert.NoError(t, err)
+	// convert to UTC:
+	d = d.UTC()
+	assert.Equal(t, "2021-01-01T05:00:00Z", d.Format("2006-01-02T15:04:05Z"))
+}
+
+func Test_ParseDate2(t *testing.T) {
+	d, err := wst.ParseDate("2021-01-01T00:00:00.000+0600")
+	assert.NoError(t, err)
+	// convert to UTC:
+	d = d.UTC()
+	assert.Equal(t, "2020-12-31T18:00:00Z", d.Format("2006-01-02T15:04:05Z"))
+}
+
+func Test_ParseDate3(t *testing.T) {
+	d, err := wst.ParseDate("2021-01-01T00:00:00Z")
+	assert.NoError(t, err)
+	assert.Equal(t, int64(1609459200), d.Unix())
+}
+
+func Test_ParseDate4(t *testing.T) {
+	d, err := wst.ParseDate("2021-01-01T00:00:00.000Z")
+	assert.NoError(t, err)
+	assert.Equal(t, "2021-01-01T00:00:00Z", d.Format("2006-01-02T15:04:05Z"))
+}
+
+func Test_ParseDateWithImpossibleDate(t *testing.T) {
+	_, err := wst.ParseDate("2021-02-29T00:00:00Z")
+	assert.Error(t, err)
+}
+
+// test CreateError(fiberError *fiber.Error, code string, details fiber.Map, name string)
+func Test_CreateError(t *testing.T) {
+	err := wst.CreateError(fiber.ErrBadRequest, "BAD_REQUEST", fiber.Map{"message": "You sent a bad request"}, "Error")
+	assert.Equal(t, "400 Bad Request: {\"message\":\"You sent a bad request\"}", err.Error())
+}
 
 func Test_AFromGenericSlice_ValidEntries(t *testing.T) {
 	slice := &[]interface{}{wst.M{"a": 1}, wst.M{"a": 2}, wst.M{"a": 3}}
