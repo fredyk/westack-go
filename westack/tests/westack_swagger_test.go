fffaadd7e96edf28dc333e4dad589cef591b8961
Jhon Magdalena Vila <6827508+fredyk@users.noreply.github.com>
2023-02-25 19:24:53 +0100

    ADD: swagger test

---
+++ westack/tests/westack_swagger_test.go
@@ -0,0 +1,47 @@
+package tests
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"testing"
+
+	wst "github.com/fredyk/westack-go/westack/common"
+)
+
+func Test_Get_Swagger_Docs(t *testing.T) {
+
+	// start client
+	client := http.Client{}
+
+	// test for error
+	res, err := client.Get("http://localhost:8020/swagger/doc.json")
+	if err != nil {
+		t.Errorf("Get Swagger Error: %s", err)
+		return
+	}
+
+	if res.StatusCode != 200 {
+		t.Errorf("Get Swagger Error: %d", res.StatusCode)
+		return
+	}
+
+	// read response
+	var out wst.M
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("Get Swagger Error: %s", err)
+		return
+	}
+	err = json.Unmarshal(body, &out)
+	if err != nil {
+		t.Errorf("Get Swagger Error: %s", err)
+		return
+	}
+
+	if out["openapi"] != "3.0.1" {
+		t.Errorf("Invalid openapi version: %s", out["openapi"])
+		return
+	}
+
+}
