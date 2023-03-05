59571eb53d42fccf6389e1223aaceb7c79deec88
Jhon Magdalena <jfredy.magdalena@gmail.com>
2023-03-05 13:43:10 +0100

    ADD: dynamic swagger ui

--- westack/tests/westack_swagger_test.go
+++ westack/tests/westack_swagger_test.go
@@ -45,3 +45,33 @@ func Test_Get_Swagger_Docs(t *testing.T) {
 	}
 
 }
+
+func Test_Get_Swagger_UI(t *testing.T) {
+
+	// start client
+	client := http.Client{}
+
+	// test for error
+	res, err := client.Get("http://localhost:8020/swagger/")
+	if err != nil {
+		t.Errorf("Get Swagger Error: %s", err)
+		return
+	}
+
+	if res.StatusCode != 200 {
+		t.Errorf("Get Swagger Error: %d", res.StatusCode)
+		return
+	}
+
+	// read response
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("Get Swagger Error: %s", err)
+		return
+	}
+
+	if len(body) == 0 {
+		t.Errorf("Get Swagger Error: empty response")
+		return
+	}
+}
