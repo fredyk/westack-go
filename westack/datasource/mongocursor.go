a33046ac86e414dfcc5001156d73fc5185dcd738
Jhon Magdalena Vila <6827508+fredyk@users.noreply.github.com>
2023-04-13 17:21:26 +0200

    FIX: Error on cursor.All()

--- westack/datasource/mongocursor.go
+++ westack/datasource/mongocursor.go
@@ -36,37 +36,74 @@ func (cursor *fixedMongoCursor) Decode(val interface{}) error {
 }
 
 func (cursor *fixedMongoCursor) All(ctx context.Context, results interface{}) error {
-	// Unmarshal all the raw inputs
-	// Treat results as a slice, grow it as needed
-	if v, ok := results.(*[]interface{}); ok {
-		for idx, rawInput := range cursor.rawInputs {
-			// Grow the slice if needed, respecting it's type
-			if idx >= len(*v) {
-				// Get the type of the slice
-				sliceType := reflect.TypeOf(*v)
-				// Get the type of the elements of the slice
-				elemType := sliceType.Elem()
-				// Create a new element of the slice's type
-				newElem := reflect.New(elemType)
-				// Unmarshal the raw input into the new element
-				err := bson.Unmarshal(rawInput, newElem.Interface())
-				if err != nil {
-					return err
-				}
-				// Append the new element to the slice
-				*v = append(*v, newElem.Elem().Interface())
-			} else {
-				// Unmarshal the raw input into the existing element
-				err := bson.Unmarshal(rawInput, (*v)[idx])
-				if err != nil {
-					return err
-				}
-			}
 
-		}
-	} else {
+	resultsVal := reflect.ValueOf(results)
+	if resultsVal.Kind() != reflect.Ptr {
+		return errors.New("results is not a pointer")
+	}
+
+	// Dereference the pointer
+	resultsVal = resultsVal.Elem()
+
+	// Check if the pointer is to a slice
+	if resultsVal.Kind() != reflect.Slice {
 		return errors.New("results is not a pointer to a slice")
 	}
+
+	// Get the slice's type
+	sliceType := resultsVal.Type()
+
+	// Get the slice's element type
+	elemType := sliceType.Elem()
+
+	// Create a new slice
+	newSlice := reflect.MakeSlice(sliceType, 0, 0)
+
+	//// Unmarshal all the raw inputs
+	//// Treat results as a slice, grow it as needed
+	//if v, ok := results.(*[]interface{}); ok {
+	for _, rawInput := range cursor.rawInputs {
+		//// Grow the slice if needed, respecting it's type
+		//if idx >= len(*v) {
+		//	// Get the type of the slice
+		//	sliceType := reflect.TypeOf(*v)
+		//	// Get the type of the elements of the slice
+		//	elemType := sliceType.Elem()
+		//	// Create a new element of the slice's type
+		//	newElem := reflect.New(elemType)
+		//	// Unmarshal the raw input into the new element
+		//	err := bson.Unmarshal(rawInput, newElem.Interface())
+		//	if err != nil {
+		//		return err
+		//	}
+		//	// Append the new element to the slice
+		//	*v = append(*v, newElem.Elem().Interface())
+		//} else {
+		//	// Unmarshal the raw input into the existing element
+		//	err := bson.Unmarshal(rawInput, (*v)[idx])
+		//	if err != nil {
+		//		return err
+		//	}
+		//}
+
+		// Create a new element of the slice's type
+		newElem := reflect.New(elemType)
+		// Unmarshal the raw input into the new element
+		err := bson.Unmarshal(rawInput, newElem.Interface())
+		if err != nil {
+			return err
+		}
+		// Append the new element to the slice
+		newSlice = reflect.Append(newSlice, newElem.Elem())
+
+	}
+	//} else {
+	//	return errors.New("results is not a pointer to a slice")
+	//}
+
+	// Set the results to the new slice
+	resultsVal.Set(newSlice)
+
 	return nil
 }
 
