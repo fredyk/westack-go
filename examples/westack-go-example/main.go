e1736179df76b0fd03570643e4f32fe943ebaf92
Jhon Magdalena Vila <6827508+fredyk@users.noreply.github.com>
2023-05-16 10:24:34 +0200

    ADD: DEV Community example 0

---
+++ examples/westack-go-example/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/fredyk/westack-go/westack"
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+func main() {
+
+	// Instantiate a new WeStack app
+	app := westack.New()
+
+	// Boot the app with a custom boot function
+	app.Boot(func(app *westack.WeStack) {
+
+		// Add a custom route
+		app.Server.Get("/status", func(c *fiber.Ctx) error {
+			return c.JSON(fiber.Map{"status": "ok"})
+		})
+
+	})
+
+	// Start the app
+	log.Fatal(app.Start())
+
+}
