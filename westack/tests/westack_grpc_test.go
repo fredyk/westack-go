74884f614a78ebd3b4bd2bd0f9b079fc186df699
Jhon Magdalena <jfmv.vt@gmail.com>
2023-08-28 14:32:44 +0200

    ADD: Some tests for special date filters

--- westack/tests/westack_grpc_test.go
+++ westack/tests/westack_grpc_test.go
@@ -124,17 +124,17 @@ func Test_GRPCCallWithBodyParamsOK(t *testing.T) {
 
 	// test for ok
 	res, err := client.Post("http://localhost:8020/test-grpc-post", "application/json", bufio.NewReader(strings.NewReader(`{"foo":1}`)))
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 	assert.Equal(t, 200, res.StatusCode)
 
 	// read response
 	body, err := io.ReadAll(res.Body)
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 
 	// compare response
 	var out pb.ResGrpcTestMessage
 	err = json.Unmarshal(body, &out)
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 	assert.Equal(t, int32(1), out.Bar)
 
 }
@@ -148,7 +148,7 @@ func Test_GRPCCallWithBodyParamsError(t *testing.T) {
 
 	// test for error
 	res, err := client.Post("http://localhost:8020/test-grpc-post", "application/json", bufio.NewReader(strings.NewReader(`{"foo":"abc"}`)))
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 	assert.Equal(t, 500, res.StatusCode)
 
 }
@@ -162,7 +162,7 @@ func Test_GRPCCallWithBodyParams_WithBadBody(t *testing.T) {
 
 	// test for error
 	res, err := client.Post("http://localhost:8020/test-grpc-post", "application/json", bufio.NewReader(strings.NewReader(`{"foo":abc}`)))
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 	assert.Equal(t, 500, res.StatusCode)
 
 }
@@ -231,6 +231,256 @@ func Test_ResGrpcTestMessage(t *testing.T) {
 	// just invoke the method to increase coverage
 	in.ProtoMessage()
 
+}
+
+func Test_SpecialFilterNow(t *testing.T) {
+
+	t.Parallel()
+
+	// Create a note for checking the filter
+	randomTitle := fmt.Sprintf("Test_SpecialFilterNow_%d", createRandomInt())
+	note, err := noteModel.Create(wst.M{
+		"title": randomTitle,
+	}, systemContext)
+	assert.NoError(t, err)
+	assert.NotNil(t, note)
+	assert.Contains(t, note.ToJSON(), "id")
+
+	notes, err := testSpecialDatePlaceholder(t, "$lte", "$now")
+	assert.NoError(t, err)
+	assert.NotEmptyf(t, notes, "There should be at least one note")
+	assert.Contains(t, reduceByKey(notes, "title"), randomTitle)
+
+}
+
+func Test_SpecialFilterToday(t *testing.T) {
+
+	t.Parallel()
+
+	// Create a note for checking the filter
+	randomTitle := fmt.Sprintf("Test_SpecialFilterToday_%d", createRandomInt())
+	note, err := noteModel.Create(wst.M{
+		// Created 24 hours ago
+		"created": time.Now().Add(-24 * time.Hour),
+		"title":   randomTitle,
+	}, systemContext)
+	assert.NoError(t, err)
+	assert.NotNil(t, note)
+	assert.Contains(t, note.ToJSON(), "id")
+
+	notes, err := testSpecialDatePlaceholder(t, "$lte", "$today")
+	assert.NoError(t, err)
+	assert.NotEmptyf(t, notes, "There should be at least one note")
+	assert.Contains(t, reduceByKey(notes, "title"), randomTitle)
+
+}
+
+func Test_SpecialFilterYesterday(t *testing.T) {
+
+	t.Parallel()
+
+	// Create a note for checking the filter
+	randomTitle := fmt.Sprintf("Test_SpecialFilterYesterday_%d", createRandomInt())
+	note, err := noteModel.Create(wst.M{
+		// Created 48 hours ago
+		"created": time.Now().Add(-48 * time.Hour),
+		"title":   randomTitle,
+	}, systemContext)
+	assert.NoError(t, err)
+	assert.NotNil(t, note)
+	assert.Contains(t, note.ToJSON(), "id")
+
+	notes, err := testSpecialDatePlaceholder(t, "$lte", "$yesterday")
+	assert.NoError(t, err)
+	assert.NotEmptyf(t, notes, "There should be at least one note")
+	assert.Contains(t, reduceByKey(notes, "title"), randomTitle)
+
+}
+
+func Test_SpecialFilter7DaysAgo(t *testing.T) {
+
+	t.Parallel()
+
+	// Create a note for checking the filter
+	randomTitle := fmt.Sprintf("Test_SpecialFilter7DaysAgo_%d", createRandomInt())
+	note, err := noteModel.Create(wst.M{
+		// Created 8 days ago
+		"created": time.Now().Add(-8 * 24 * time.Hour),
+		"title":   randomTitle,
+	}, systemContext)
+	assert.NoError(t, err)
+	assert.NotNil(t, note)
+	assert.Contains(t, note.ToJSON(), "id")
+
+	notes, err := testSpecialDatePlaceholder(t, "$lte", "$7dago")
+	assert.NoError(t, err)
+	assert.NotEmptyf(t, notes, "There should be at least one note")
+	assert.Contains(t, reduceByKey(notes, "title"), randomTitle)
+
+}
+
+func Test_SpecialFilter4WeeksAgo(t *testing.T) {
+
+	t.Parallel()
+
+	// Create a note for checking the filter
+	randomTitle := fmt.Sprintf("Test_SpecialFilter4WeeksAgo_%d", createRandomInt())
+	note, err := noteModel.Create(wst.M{
+		// Created 5 weeks ago
+		"created": time.Now().Add(-5 * 7 * 24 * time.Hour),
+		"title":   randomTitle,
+	}, systemContext)
+	assert.NoError(t, err)
+	assert.NotNil(t, note)
+	assert.Contains(t, note.ToJSON(), "id")
+
+	notes, err := testSpecialDatePlaceholder(t, "$lte", "$4wago")
+	assert.NoError(t, err)
+	assert.NotEmptyf(t, notes, "There should be at least one note")
+	assert.Contains(t, reduceByKey(notes, "title"), randomTitle)
+
+}
+
+func Test_SpecialFilter3MonthsAgo(t *testing.T) {
+
+	t.Parallel()
+
+	// Create a note for checking the filter
+	randomTitle := fmt.Sprintf("Test_SpecialFilter3MonthsAgo_%d", createRandomInt())
+	note, err := noteModel.Create(wst.M{
+		// Created 4 months ago
+		"created": time.Now().Add(-4 * 30 * 24 * time.Hour),
+		"title":   randomTitle,
+	}, systemContext)
+	assert.NoError(t, err)
+	assert.NotNil(t, note)
+	assert.Contains(t, note.ToJSON(), "id")
+
+	notes, err := testSpecialDatePlaceholder(t, "$lte", "$3Mago")
+	assert.NoError(t, err)
+	assert.NotEmptyf(t, notes, "There should be at least one note")
+	assert.Contains(t, reduceByKey(notes, "title"), randomTitle)
+
+}
+
+func Test_SpecialFilter2YearsAgo(t *testing.T) {
+
+	t.Parallel()
+
+	// Create a note for checking the filter
+	randomTitle := fmt.Sprintf("Test_SpecialFilter2YearsAgo_%d", createRandomInt())
+	note, err := noteModel.Create(wst.M{
+		// Created 3 years ago
+		"created": time.Now().Add(-3 * 365 * 24 * time.Hour),
+		"title":   randomTitle,
+	}, systemContext)
+	assert.NoError(t, err)
+	assert.NotNil(t, note)
+	assert.Contains(t, note.ToJSON(), "id")
+
+	notes, err := testSpecialDatePlaceholder(t, "$lte", "$2yago")
+	assert.NoError(t, err)
+	assert.NotEmptyf(t, notes, "There should be at least one note")
+	assert.Contains(t, reduceByKey(notes, "title"), randomTitle)
+
+}
+
+func Test_SpecialFilter15SecondsAgo(t *testing.T) {
+
+	t.Parallel()
+
+	// Create a note for checking the filter
+	randomTitle := fmt.Sprintf("Test_SpecialFilter15SecondsAgo_%d", createRandomInt())
+	note, err := noteModel.Create(wst.M{
+		// Created 16 seconds ago
+		"created": time.Now().Add(-16 * time.Second),
+		"title":   randomTitle,
+	}, systemContext)
+	assert.NoError(t, err)
+	assert.NotNil(t, note)
+	assert.Contains(t, note.ToJSON(), "id")
+
+	notes, err := testSpecialDatePlaceholder(t, "$lte", "$15Sago")
+	assert.NoError(t, err)
+	assert.NotEmptyf(t, notes, "There should be at least one note")
+	assert.Contains(t, reduceByKey(notes, "title"), randomTitle)
+
+}
+
+func Test_SpecialFilter10MinutesAgo(t *testing.T) {
+
+	t.Parallel()
+
+	// Create a note for checking the filter
+	randomTitle := fmt.Sprintf("Test_SpecialFilter10MinutesAgo_%d", createRandomInt())
+	note, err := noteModel.Create(wst.M{
+		// Created 11 minutes ago
+		"created": time.Now().Add(-11 * time.Minute),
+		"title":   randomTitle,
+	}, systemContext)
+	assert.NoError(t, err)
+	assert.NotNil(t, note)
+	assert.Contains(t, note.ToJSON(), "id")
+
+	notes, err := testSpecialDatePlaceholder(t, "$lte", "$10Mago")
+	assert.NoError(t, err)
+	assert.NotEmptyf(t, notes, "There should be at least one note")
+	assert.Contains(t, reduceByKey(notes, "title"), randomTitle)
+
+}
+
+func Test_SpecialFilter5HoursAgo(t *testing.T) {
+
+	t.Parallel()
+
+	// Create a note for checking the filter
+	randomTitle := fmt.Sprintf("Test_SpecialFilter5HoursAgo_%d", createRandomInt())
+	note, err := noteModel.Create(wst.M{
+		// Created 6 hours ago
+		"created": time.Now().Add(-6 * time.Hour),
+		"title":   randomTitle,
+	}, systemContext)
+	assert.NoError(t, err)
+	assert.NotNil(t, note)
+	assert.Contains(t, note.ToJSON(), "id")
+
+	notes, err := testSpecialDatePlaceholder(t, "$lte", "$5Hago")
+	assert.NoError(t, err)
+	assert.NotEmptyf(t, notes, "There should be at least one note")
+	assert.Contains(t, reduceByKey(notes, "title"), randomTitle)
+
+}
+
+func Test_SpecialFilterTomorrow(t *testing.T) {
+
+	t.Parallel()
+
+	// Create a note for checking the filter
+	randomTitle := fmt.Sprintf("Test_SpecialFilterTomorrow_%d", createRandomInt())
+	note, err := noteModel.Create(wst.M{
+		"title": randomTitle,
+	}, systemContext)
+	assert.NoError(t, err)
+	assert.NotNil(t, note)
+	assert.Contains(t, note.ToJSON(), "id")
+
+	notes, err := testSpecialDatePlaceholder(t, "$lte", "$tomorrow")
+	assert.NoError(t, err)
+	assert.NotEmptyf(t, notes, "There should be at least one note")
+	assert.Contains(t, reduceByKey(notes, "title"), randomTitle)
+
+}
+
+func testSpecialDatePlaceholder(t *testing.T, specialDateKey string, specialDatePlaceholder string) (wst.A, error) {
+	req, err := http.NewRequest("GET", "/api/v1/notes?filter={\"where\":{\"created\":{\""+specialDateKey+"\":\""+specialDatePlaceholder+"\"}}}", nil)
+	assert.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := app.Server.Test(req, -1)
+	assert.NoError(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
+	var out wst.A
+	err = json.NewDecoder(resp.Body).Decode(&out)
+	return out, err
 }
 
 // before all tests
