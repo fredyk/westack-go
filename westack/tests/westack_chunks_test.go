3d363ba42d6ffe204d435cc13c3c308fa169eb07
Jhon Magdalena <jfmv.vt@gmail.com>
2023-08-31 12:00:14 +0200

    UPDATE: Multiple changes in tests and automatic dependency updates

--- westack/tests/westack_chunks_test.go
+++ westack/tests/westack_chunks_test.go
@@ -1,8 +1,10 @@
 package tests
 
 import (
+	"encoding/json"
 	"errors"
 	"io"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -93,5 +95,108 @@ func Test_ChannelChunkGeneratorClosedError(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(t, 1, len(outBytes))
 	assert.Equal(t, byte('['), outBytes[0])
+
+}
+
+func Test_FixedBeforeLoadMock124401(t *testing.T) {
+
+	t.Parallel()
+
+	request, err := http.NewRequest("GET", "/api/v1/notes?mockResultTest124401=true", nil)
+	assert.NoError(t, err)
+	request.Header.Set("Accept", "application/json")
+	request.Header.Set("Content-Type", "application/json")
+	resp, err := executeRequest(request)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	responseBody, err := parseResultAsJsonArray(resp)
+	assert.NoError(t, err)
+	assert.NotNil(t, responseBody)
+	assert.Equal(t, 1, len(responseBody))
+	assert.Equal(t, "mocked124401", responseBody[0].GetString("title"))
+
+}
+
+func Test_FixedBeforeLoadMock124402(t *testing.T) {
+
+	t.Parallel()
+
+	request, err := http.NewRequest("GET", "/api/v1/notes?mockResultTest124402=true", nil)
+	assert.NoError(t, err)
+	request.Header.Set("Accept", "application/json")
+	request.Header.Set("Content-Type", "application/json")
+	resp, err := executeRequest(request)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	responseBody, err := parseResultAsJsonArray(resp)
+	assert.NoError(t, err)
+	assert.NotNil(t, responseBody)
+	assert.Equal(t, 1, len(responseBody))
+	assert.Equal(t, "mocked124402", responseBody[0].GetString("title"))
+
+}
+
+func Test_FixedBeforeLoadMock124403(t *testing.T) {
+
+	t.Parallel()
+
+	request, err := http.NewRequest("GET", "/api/v1/notes?mockResultTest124403=true", nil)
+	assert.NoError(t, err)
+	request.Header.Set("Accept", "application/json")
+	request.Header.Set("Content-Type", "application/json")
+	resp, err := executeRequest(request)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	responseBody, err := parseResultAsJsonArray(resp)
+	assert.NoError(t, err)
+	assert.NotNil(t, responseBody)
+	assert.Equal(t, 1, len(responseBody))
+	assert.Equal(t, "mocked124403", responseBody[0].GetString("title"))
+
+}
+
+func Test_FixedBeforeLoadMock124404(t *testing.T) {
+
+	t.Parallel()
+
+	request, err := http.NewRequest("GET", "/api/v1/notes?mockResultTest124404=true", nil)
+	assert.NoError(t, err)
+	request.Header.Set("Accept", "application/json")
+	request.Header.Set("Content-Type", "application/json")
+	resp, err := executeRequest(request)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	responseBody, err := parseResultAsJsonArray(resp)
+	assert.NoError(t, err)
+	assert.NotNil(t, responseBody)
+	assert.Equal(t, 1, len(responseBody))
+	assert.Equal(t, "mocked124404", responseBody[0].GetString("title"))
+
+}
+
+func parseResultAsJsonArray(resp *http.Response) (responseBody wst.A, err error) {
+
+	bytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(bytes, &responseBody)
+	return responseBody, err
+
+}
+
+func executeRequest(request *http.Request) (*http.Response, error) {
+
+	return executeRequestRaw(request)
+
+}
+
+func executeRequestRaw(request *http.Request) (*http.Response, error) {
+
+	resp, err := app.Server.Test(request)
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
 
 }
