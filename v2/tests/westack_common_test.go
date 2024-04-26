172a0a8d10f1f686654fdf062156f8d3af76f535
fred <jfmv.vt@gmail.com>
2024-04-26 16:02:32 +0000

    ADD: lambdas

---
+++ v2/tests/westack_common_test.go
@@ -0,0 +1,391 @@
+package tests
+
+import (
+	"github.com/mailru/easyjson"
+	"math"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	wst "github.com/fredyk/westack-go/v2/common"
+)
+
+func Test_GetM(t *testing.T) {
+
+	t.Parallel()
+
+	m := wst.M{"a": wst.M{"b": "z"}}
+	assert.Equal(t, "z", m.GetM("a").GetString("b"))
+}
+
+func Test_GetM_WithNativeMap(t *testing.T) {
+
+	t.Parallel()
+
+	m := wst.M{"a": map[string]interface{}{"b": "z"}}
+	assert.Equal(t, "z", m.GetM("a").GetString("b"))
+}
+
+func Test_GetM_NonexistentKey(t *testing.T) {
+
+	t.Parallel()
+
+	m := wst.M{"a": wst.M{"b": "z"}}
+	assert.Nil(t, m.GetM("c"))
+}
+
+func Test_GetM_NilInput(t *testing.T) {
+
+	t.Parallel()
+
+	var m wst.M = nil
+	assert.Nil(t, m.GetM("a"))
+}
+
+func Test_GetString_NilMap(t *testing.T) {
+
+	t.Parallel()
+
+	var m wst.M = nil
+	assert.Equal(t, "", m.GetString("a"))
+}
+
+func Test_GetString_NonexistentKey(t *testing.T) {
+
+	t.Parallel()
+
+	m := wst.M{"a": wst.M{"b": "z"}}
+	assert.Equal(t, "", m.GetString("c"))
+}
+
+func Test_Error(t *testing.T) {
+
+	t.Parallel()
+
+	err := wst.WeStackError{
+		FiberError: fiber.ErrUnauthorized,
+		Code:       "UNAUTHORIZED",
+		Details:    fiber.Map{"message": "Unauthorized"},
+		Name:       "Error",
+	}
+	// test err.Error()
+	assert.Equal(t, "401 Unauthorized: {\"message\":\"Unauthorized\"}", err.Error())
+}
+
+func Test_Error_WithErorOnMarshalDetails(t *testing.T) {
+
+	t.Parallel()
+
+	err := wst.WeStackError{
+		FiberError: fiber.ErrUnauthorized,
+		Code:       "UNAUTHORIZED",
+		Details:    fiber.Map{"message": make(chan int)},
+		Name:       "Error",
+	}
+	// test err.Error()
+	assert.Regexpf(t, "401 Unauthorized: map\\[message:0x[0-9a-f]+\\]", err.Error(), "Error message should contain the address of the channel")
+}
+
+func Test_LoadFile_InvalidPath(t *testing.T) {
+
+	t.Parallel()
+
+	var m wst.M
+	err := wst.LoadFile("invalid_path", &m)
+	assert.Error(t, err)
+}
+
+func Test_LoadFile_InvalidJson(t *testing.T) {
+
+	t.Parallel()
+
+	var m wst.M
+	// write the json:
+	err := os.WriteFile("invalid.json", []byte("invalid json"), 0600)
+	if err != nil {
+		t.Errorf("Error writing file: %v", err)
+		return
+	}
+	// clean up:
+	defer func() {
+		err := os.Remove("invalid.json")
+		if err != nil {
+			t.Errorf("Error deleting file: %v", err)
+		}
+	}()
+	err = wst.LoadFile("invalid.json", &m)
+
+	assert.Error(t, err)
+}
+
+func Test_DashedCase(t *testing.T) {
+
+	t.Parallel()
+
+	assert.Equal(t, "hello-world", wst.DashedCase("helloWorld"))
+}
+
+func Test_Transform(t *testing.T) {
+
+	t.Parallel()
+
+	type OutputType struct {
+		Foo string `json:"foo"`
+	}
+	inputMap := wst.M{"foo": "bar"}
+	var output OutputType
+	err := wst.Transform(inputMap, &output)
+	assert.NoError(t, err)
+	assert.Equal(t, "bar", output.Foo)
+}
+
+func Test_Transform_WithOutputError(t *testing.T) {
+
+	t.Parallel()
+
+	type OutputType struct {
+		Foo chan int `json:"foo"`
+	}
+	inputMap := wst.M{"foo": "bar"}
+	var output OutputType
+	err := wst.Transform(inputMap, &output)
+	assert.Error(t, err)
+}
+
+func Test_Transform_WithInputError(t *testing.T) {
+
+	t.Parallel()
+
+	type OutputType struct {
+		Foo string `json:"foo"`
+	}
+	inputMap := wst.M{"foo": make(chan int)}
+	var output OutputType
+	err := wst.Transform(inputMap, &output)
+	assert.Error(t, err)
+}
+
+func Test_ParseDate1(t *testing.T) {
+
+	t.Parallel()
+
+	d, err := wst.ParseDate("2021-01-01T00:00:00-0500")
+	assert.NoError(t, err)
+	// convert to UTC:
+	d = d.UTC()
+	assert.Equal(t, "2021-01-01T05:00:00Z", d.Format("2006-01-02T15:04:05Z"))
+}
+
+func Test_ParseDate2(t *testing.T) {
+
+	t.Parallel()
+
+	d, err := wst.ParseDate("2021-01-01T00:00:00.000+0600")
+	assert.NoError(t, err)
+	// convert to UTC:
+	d = d.UTC()
+	assert.Equal(t, "2020-12-31T18:00:00Z", d.Format("2006-01-02T15:04:05Z"))
+}
+
+func Test_ParseDate3_00(t *testing.T) {
+
+	t.Parallel()
+
+	d, err := wst.ParseDate("2021-01-01T00:00:00Z")
+	assert.NoError(t, err)
+	assert.Equal(t, int64(1609459200), d.Unix())
+}
+
+func Test_ParseDate3_01(t *testing.T) {
+
+	t.Parallel()
+
+	d, err := wst.ParseDate("1967-11-07T00:00:00")
+	assert.NoError(t, err)
+	assert.Equal(t, int64(-67910400), d.Unix())
+}
+
+func Test_ParseDate4(t *testing.T) {
+
+	t.Parallel()
+
+	d, err := wst.ParseDate("2021-01-01T00:00:00.000Z")
+	assert.NoError(t, err)
+	assert.Equal(t, "2021-01-01T00:00:00Z", d.Format("2006-01-02T15:04:05Z"))
+}
+
+func Test_ParseDateWithImpossibleDate(t *testing.T) {
+
+	t.Parallel()
+
+	_, err := wst.ParseDate("2021-02-29T00:00:00Z")
+	assert.Error(t, err)
+}
+
+// test CreateError(fiberError *fiber.Error, code string, details fiber.Map, name string)
+func Test_CreateError(t *testing.T) {
+
+	t.Parallel()
+
+	err := wst.CreateError(fiber.ErrBadRequest, "BAD_REQUEST", fiber.Map{"message": "You sent a bad request"}, "Error")
+	assert.Equal(t, "400 Bad Request: {\"message\":\"You sent a bad request\"}", err.Error())
+}
+
+func Test_AFromGenericSlice_ValidEntries(t *testing.T) {
+
+	t.Parallel()
+
+	slice := &[]interface{}{wst.M{"a": 1}, wst.M{"a": 2}, wst.M{"a": 3}}
+	var a = *wst.AFromGenericSlice(slice)
+	assert.Equal(t, 3, len(a))
+	assert.Equal(t, 1, a[0]["a"])
+	assert.Equal(t, 2, a[1]["a"])
+	assert.Equal(t, 3, a[2]["a"])
+}
+
+func Test_AFromGenericSlice_NilInput(t *testing.T) {
+
+	t.Parallel()
+
+	var a = wst.AFromGenericSlice(nil)
+	assert.Nil(t, a)
+}
+
+func Test_AFromGenericSlice_WrongType(t *testing.T) {
+
+	t.Parallel()
+
+	slice := &[]interface{}{wst.M{"a": 1}, map[string]interface{}{"a": 3}}
+	var a = *wst.AFromGenericSlice(slice)
+	assert.Equal(t, 2, len(a))
+	assert.Equal(t, 1, a[0]["a"])
+	// Ensure the second 1 is an empty wst.M
+	assert.Equal(t, 0, len(a[1]))
+}
+
+func Test_AFromPrimitiveSlice_ValidEntries(t *testing.T) {
+
+	t.Parallel()
+
+	slice := &primitive.A{primitive.M{"a": 1}, primitive.M{"a": 2}, primitive.M{"a": 3}}
+	var a = *wst.AFromPrimitiveSlice(slice)
+	assert.Equal(t, 3, len(a))
+	assert.Equal(t, 1, a[0]["a"])
+	assert.Equal(t, 2, a[1]["a"])
+	assert.Equal(t, 3, a[2]["a"])
+}
+
+func Test_AFromPrimitiveSlice_ValidEntriesAsM(t *testing.T) {
+
+	t.Parallel()
+
+	var slice *primitive.A = &primitive.A{wst.M{"a": 1}, wst.M{"a": 2}, wst.M{"a": 3}}
+	var a = *wst.AFromPrimitiveSlice(slice)
+	assert.Equal(t, 3, len(a))
+	assert.Equal(t, 1, a[0]["a"])
+	assert.Equal(t, 2, a[1]["a"])
+	assert.Equal(t, 3, a[2]["a"])
+}
+
+func Test_AFromPrimitiveSlice_NilInput(t *testing.T) {
+
+	t.Parallel()
+
+	var a = wst.AFromPrimitiveSlice(nil)
+	assert.Nil(t, a)
+}
+
+func Test_AFromPrimitiveSlice_WrongType(t *testing.T) {
+
+	t.Parallel()
+
+	var slice *primitive.A = &primitive.A{primitive.M{"a": 1}, wst.M{"a": 2}, map[string]interface{}{"a": 3}}
+	var a = *wst.AFromPrimitiveSlice(slice)
+	assert.Equal(t, 3, len(a))
+	assert.Equal(t, 1, a[0]["a"])
+	assert.Equal(t, 2, a[1]["a"])
+	// Ensure the second 1 is an empty wst.M
+	assert.Equal(t, 0, len(a[2]))
+}
+
+func Test_MMarshalEasyJSON(t *testing.T) {
+
+	t.Parallel()
+
+	m := wst.M{"a": 1, "b": "2"}
+	b, err := easyjson.Marshal(&m)
+	assert.NoError(t, err)
+	assert.Contains(t, string(b), `"a":1`)
+	assert.Contains(t, string(b), `"b":"2"`)
+}
+
+func Test_MMarshalEasyJSONNil(t *testing.T) {
+
+	t.Parallel()
+
+	var m *wst.M
+	b, err := easyjson.Marshal(m)
+	assert.NoError(t, err)
+	assert.Equal(t, `null`, string(b))
+}
+
+func Test_MMarshalEasyJSONSpecialNumbers(t *testing.T) {
+
+	t.Parallel()
+
+	m := wst.M{
+		"a": math.Inf(1),
+		"b": math.Inf(-1),
+		"c": math.NaN(),
+		"d": uint8(1),
+		"e": uint16(2),
+		"f": uint32(3),
+		"g": uint64(4),
+		"h": int8(5),
+		"i": int16(6),
+		"j": int32(7),
+		"k": int64(8),
+		"l": float32(9),
+		"m": float64(10),
+		"n": true,
+	}
+	b, err := easyjson.Marshal(&m)
+	assert.NoError(t, err)
+	assert.Contains(t, string(b), `"a":+Inf`)
+	assert.Contains(t, string(b), `"b":-Inf`)
+	assert.Contains(t, string(b), `"c":NaN`)
+	assert.Contains(t, string(b), `"d":1`)
+	assert.Contains(t, string(b), `"e":2`)
+	assert.Contains(t, string(b), `"f":3`)
+	assert.Contains(t, string(b), `"g":4`)
+	assert.Contains(t, string(b), `"h":5`)
+	assert.Contains(t, string(b), `"i":6`)
+	assert.Contains(t, string(b), `"j":7`)
+	assert.Contains(t, string(b), `"k":8`)
+	assert.Contains(t, string(b), `"l":9`)
+	assert.Contains(t, string(b), `"m":10`)
+	assert.Contains(t, string(b), `"n":true`)
+
+}
+
+func Test_MMarshalEasyJSONDateTime(t *testing.T) {
+
+	t.Parallel()
+
+	m := wst.M{"a": primitive.NewDateTimeFromTime(time.Date(2023, 2, 28, 19, 42, 42, 824000000, time.UTC))}
+	b, err := easyjson.Marshal(&m)
+	assert.NoError(t, err)
+
+	// The time is converted to the local timezone
+	// if the time is not in UTC:
+	_, offset := time.Now().Zone()
+	if offset != 0 {
+		assert.Contains(t, string(b), `"a":"2023-02-28T20:42:42.824+01:00"`)
+	} else {
+		assert.Contains(t, string(b), `"a":"2023-02-28T19:42:42.824Z"`)
+	}
+}
---
+++ v2/tests/westack_model_test.go
@@ -0,0 +1,560 @@
+package tests
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/goccy/go-json"
+
+	"github.com/fredyk/westack-go/v2"
+	"github.com/fredyk/westack-go/v2/model"
+
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	wst "github.com/fredyk/westack-go/v2/common"
+)
+
+func Test_CreateWithMap(t *testing.T) {
+
+	t.Parallel()
+
+	created, err := noteModel.Create(map[string]interface{}{
+		"title": "Test",
+	}, systemContext)
+	assert.NoError(t, err)
+	assert.Equal(t, "Test", created.GetString("title"))
+}
+
+func Test_CreateWithMapPointer(t *testing.T) {
+
+	t.Parallel()
+
+	timeNow := time.Now()
+	created, err := noteModel.Create(&map[string]interface{}{
+		"date": timeNow,
+	}, systemContext)
+	assert.NoError(t, err)
+	assert.Equal(t, timeNow.Minute(), created.ToJSON()["date"].(primitive.DateTime).Time().Minute())
+}
+
+func Test_CreateWithStruct(t *testing.T) {
+
+	t.Parallel()
+
+	created, err := noteModel.Create(struct {
+		SomeInt int `bson:"someInt"`
+	}{
+		SomeInt: 1,
+	}, systemContext)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(1), created.GetInt("someInt"))
+}
+
+func Test_CreateWithM(t *testing.T) {
+
+	t.Parallel()
+
+	created, err := noteModel.Create(wst.M{
+		"someFloat": 1.1,
+	}, systemContext)
+	assert.NoError(t, err)
+	assert.Equal(t, 1.1, created.GetFloat64("someFloat"))
+}
+
+func Test_CreateWithMPointer(t *testing.T) {
+
+	t.Parallel()
+
+	created, err := noteModel.Create(&wst.M{
+		"someBoolean": true,
+	}, systemContext)
+	assert.NoError(t, err)
+	assert.Equal(t, true, created.GetBoolean("someBoolean", false))
+}
+
+func Test_CreateWithInstance(t *testing.T) {
+
+	t.Parallel()
+
+	build, err2 := noteModel.Build(wst.M{
+		"foo": "bar",
+	}, systemContext)
+	assert.Nil(t, err2)
+	created, err := noteModel.Create(build, systemContext)
+	assert.NoError(t, err)
+	assert.Equal(t, "bar", created.GetString("foo"))
+}
+
+func Test_CreateWithInstancePointer(t *testing.T) {
+
+	t.Parallel()
+
+	v, err := noteModel.Build(wst.M{
+		"foo2": "bar2",
+	}, systemContext)
+	assert.NoError(t, err)
+	created, err := noteModel.Create(&v, systemContext)
+	assert.NoError(t, err)
+	assert.Equal(t, "bar2", created.GetString("foo2"))
+}
+
+func Test_CreateWithBadStruct(t *testing.T) {
+
+	t.Parallel()
+
+	_, err := noteModel.Create(struct {
+		SomeInt chan int `bson:"someInt"`
+	}{
+		SomeInt: make(chan int),
+	}, systemContext)
+	assert.Error(t, err)
+}
+
+func Test_CreateWithInvalidInput(t *testing.T) {
+
+	t.Parallel()
+
+	_, err := noteModel.Create(1, systemContext)
+	assert.Error(t, err)
+}
+
+func Test_CreateWithOverrideResultAsM(t *testing.T) {
+
+	t.Parallel()
+
+	created, err := noteModel.Create(wst.M{
+		"__overwriteWith": wst.M{
+			"overrided1": true,
+		},
+	}, systemContext)
+	assert.NoError(t, err)
+	assert.Equal(t, true, created.GetBoolean("overrided1", false))
+}
+
+func Test_CreateWithOverrideResultAsInstance(t *testing.T) {
+
+	t.Parallel()
+
+	_, err := noteModel.Create(wst.M{
+		"__overwriteWithInstance": wst.M{
+			"overrided2": wst.M{
+				"overrided3": true,
+			},
+		},
+	}, systemContext)
+	assert.NoError(t, err)
+}
+
+func Test_CreateWithOverrideResultAsInstancePointer(t *testing.T) {
+
+	t.Parallel()
+
+	_, err := noteModel.Create(wst.M{
+		"__overwriteWithInstancePointer": wst.M{
+			"overrided4": wst.M{
+				"overrided5": true,
+			},
+		},
+	}, systemContext)
+	assert.NoError(t, err)
+}
+
+func Test_CreateWithOverrideResultError(t *testing.T) {
+
+	t.Parallel()
+
+	result, err := invokeApiAsRandomUser(t, "POST", "/notes", wst.M{
+		"__forceError": true,
+	}, wst.M{"Content-Type": "application/json"})
+	assert.NoError(t, err)
+	assert.Contains(t, result, "error")
+}
+
+func Test_CreateWithOverrideInvalid(t *testing.T) {
+
+	t.Parallel()
+
+	result, err := invokeApiAsRandomUser(t, "POST", "/notes", wst.M{
+		"__overwriteWith": 1,
+	}, wst.M{"Content-Type": "application/json"})
+	assert.NoError(t, err)
+	assert.Contains(t, result, "error")
+}
+
+func Test_CreateWithInvalidBsonInput(t *testing.T) {
+
+	t.Parallel()
+
+	created, err := noteModel.Create(wst.M{
+		"invalid": make(chan int),
+	}, systemContext)
+	assert.Errorf(t, err, "Should not be able to create with invalid bson <-- %v", created)
+}
+
+func Test_CreateWithForcingError(t *testing.T) {
+
+	t.Parallel()
+
+	result, err := invokeApiAsRandomUser(t, "POST", "/notes", wst.M{
+		"__forceAfterError": true,
+	}, wst.M{"Content-Type": "application/json"})
+	assert.NoError(t, err)
+	assert.Contains(t, result, "error")
+}
+
+func Test_EnforceExError(t *testing.T) {
+
+	t.Parallel()
+
+	err, _ := noteModel.EnforceEx(nil, "", "create", &model.EventContext{})
+	assert.Error(t, err)
+}
+
+func Test_BearerTokenWithObjectId(t *testing.T) {
+
+	t.Parallel()
+
+	createdNote, err := noteModel.Create(wst.M{
+		"title":  "Test",
+		"userId": randomUserToken.GetString("userId"),
+		"appId":  appInstance.Id,
+	}, systemContext)
+	assert.NoError(t, err)
+	subjId, err := primitive.ObjectIDFromHex(randomUserToken.GetString("userId"))
+	assert.NoError(t, err)
+	userBearer := model.CreateBearer(subjId, float64(time.Now().Unix()), float64(60), []string{"USER"})
+	refreshedByUser, err := noteModel.FindById(createdNote.GetID(), &wst.Filter{
+		Include: &wst.Include{
+			{Relation: "user"},
+			{Relation: "footer1"},
+			{Relation: "footer2"},
+			{Relation: "publicFooter"},
+			{Relation: "entries"},
+		},
+	}, &model.EventContext{
+		Bearer: userBearer,
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, "Test", refreshedByUser.GetString("title"))
+	updatedByUser, err := refreshedByUser.UpdateAttributes(wst.M{
+		"title": "Test2",
+	}, &model.EventContext{
+		Bearer: userBearer,
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, "Test2", updatedByUser.GetString("title"))
+
+	refreshedByApp, err := noteModel.FindById(createdNote.GetID(), &wst.Filter{
+		Include: &wst.Include{
+			{Relation: "user"},
+			{Relation: "footer1"},
+			{Relation: "footer2"},
+			{Relation: "publicFooter"},
+			{Relation: "entries"},
+		},
+	}, &model.EventContext{
+		Bearer: appBearer,
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, "Test2", refreshedByApp.GetString("title"))
+	updatedByApp, err := refreshedByApp.UpdateAttributes(wst.M{
+		"title": "Test3",
+	}, &model.EventContext{
+		Bearer: appBearer,
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, "Test3", updatedByApp.GetString("title"))
+}
+
+func Test_CreateWithDefaultStringValue(t *testing.T) {
+
+	t.Parallel()
+
+	created, err := invokeApiAsRandomUser(t, "POST", "/notes", wst.M{}, wst.M{"Content-Type": "application/json"})
+	assert.Nil(t, err)
+	assert.Equal(t, "default", created.GetString("defaultString"))
+}
+
+func Test_CreateWithDefaultIntValue(t *testing.T) {
+
+	t.Parallel()
+
+	created, err := invokeApiAsRandomUser(t, "POST", "/notes", wst.M{}, wst.M{"Content-Type": "application/json"})
+	assert.Nil(t, err)
+	assert.EqualValues(t, 1, created["defaultInt"])
+}
+
+func Test_CreateWithDefaultFloatValue(t *testing.T) {
+
+	t.Parallel()
+
+	created, err := invokeApiAsRandomUser(t, "POST", "/notes", wst.M{}, wst.M{"Content-Type": "application/json"})
+	assert.Nil(t, err)
+	assert.EqualValues(t, 87436874647.8761781676, created["defaultFloat"])
+}
+
+func Test_CreateWithDefaultBooleanValue(t *testing.T) {
+
+	t.Parallel()
+
+	created, err := invokeApiAsRandomUser(t, "POST", "/notes", wst.M{}, wst.M{"Content-Type": "application/json"})
+	assert.Nil(t, err)
+	assert.EqualValues(t, true, created["defaultBoolean"])
+}
+
+func Test_CreateWithDefaultListValue(t *testing.T) {
+
+	t.Parallel()
+
+	created, err := invokeApiAsRandomUser(t, "POST", "/notes", wst.M{}, wst.M{"Content-Type": "application/json"})
+	assert.Nil(t, err)
+	assert.Contains(t, created, "defaultList")
+	assert.IsType(t, []interface{}{}, created["defaultList"])
+	assert.EqualValues(t, []interface{}{"default"}, created["defaultList"])
+}
+
+func Test_CreateWithDefaultMapValue(t *testing.T) {
+
+	t.Parallel()
+
+	created, err := invokeApiAsRandomUser(t, "POST", "/notes", wst.M{}, wst.M{"Content-Type": "application/json"})
+	assert.Nil(t, err)
+	assert.Contains(t, created, "defaultMap")
+	assert.IsType(t, map[string]interface{}{}, created["defaultMap"])
+	assert.Contains(t, created["defaultMap"].(map[string]interface{}), "defaultKey")
+	assert.Equal(t, map[string]interface{}{"defaultKey": "defaultValue"}, created["defaultMap"].(map[string]interface{}))
+}
+
+func Test_CreateWithDefaultNilValue(t *testing.T) {
+
+	t.Parallel()
+
+	created, err := invokeApiAsRandomUser(t, "POST", "/notes", wst.M{}, wst.M{"Content-Type": "application/json"})
+	assert.Nil(t, err)
+	assert.Contains(t, created, "defaultNull")
+	assert.Nil(t, created["defaultNull"])
+}
+
+func Test_CreateWithDefaultTimeValue(t *testing.T) {
+
+	t.Parallel()
+
+	probablyTime := time.Now()
+	lowerSeconds := probablyTime.Unix()
+	// Should be 16 seconds after at most
+	upperSeconds := probablyTime.Unix() + 16
+	created, err := invokeApiAsRandomUser(t, "POST", "/notes", wst.M{}, wst.M{"Content-Type": "application/json"})
+	assert.Nil(t, err)
+	assert.Contains(t, created, "defaultTimeNow")
+	var parsedTime time.Time
+	parsedTime, err = time.Parse(time.RFC3339, created["defaultTimeNow"].(string))
+	assert.NoError(t, err)
+	assert.GreaterOrEqual(t, parsedTime.Unix(), lowerSeconds)
+	assert.LessOrEqual(t, parsedTime.Unix(), upperSeconds)
+
+}
+
+func Test_CreateWithDefaultTimeHourAgo(t *testing.T) {
+	t.Parallel()
+
+	probablyTime := time.Now()
+	lowerSeconds := probablyTime.Unix() - 3600
+	// Should be 15 milliseconds after at most
+	upperSeconds := probablyTime.Unix() - 3597
+	created, err := invokeApiAsRandomUser(t, "POST", "/notes", wst.M{}, wst.M{"Content-Type": "application/json"})
+	assert.Nil(t, err)
+	assert.Contains(t, created, "defaultTimeHourAgo")
+	var parsedTime time.Time
+	parsedTime, err = time.Parse(time.RFC3339, created["defaultTimeHourAgo"].(string))
+	assert.NoError(t, err)
+	assert.GreaterOrEqual(t, parsedTime.Unix(), lowerSeconds)
+	assert.LessOrEqual(t, parsedTime.Unix(), upperSeconds)
+
+}
+
+func Test_CreateWithDefaultTimeHourFromNow(t *testing.T) {
+
+	t.Parallel()
+
+	probablyTime := time.Now()
+	lowerSeconds := probablyTime.Unix() + 3600
+	// Should be 15 seconds after at most
+	upperSeconds := probablyTime.Unix() + 3615
+	created, err := invokeApiAsRandomUser(t, "POST", "/notes", wst.M{}, wst.M{"Content-Type": "application/json"})
+	assert.Nil(t, err)
+	assert.Contains(t, created, "defaultTimeHourFromNow")
+	var parsedTime time.Time
+	parsedTime, err = time.Parse(time.RFC3339, created["defaultTimeHourFromNow"].(string))
+	assert.NoError(t, err)
+	assert.GreaterOrEqual(t, parsedTime.Unix(), lowerSeconds)
+	assert.LessOrEqual(t, parsedTime.Unix(), upperSeconds)
+
+}
+
+// https://github.com/fredyk/westack-go/issues/464
+func Test_ProtectedFields(t *testing.T) {
+
+	t.Parallel()
+
+	// Create a random user
+	plainUser1 := wst.M{
+		"username": fmt.Sprintf("user%v", createRandomInt()),
+		"password": "abcd1234.",
+		"phone":    "1234567890",
+	}
+	user1 := createUser(t, plainUser1)
+
+	// Another random user
+	plainUser2 := wst.M{
+		"username": fmt.Sprintf("user%v", createRandomInt()),
+		"password": "abcd1234.",
+		"phone":    "9876543210",
+	}
+	createUser(t, plainUser2)
+
+	// User with special privilege "__protectedFieldsPrivileged"
+	plainUser3 := wst.M{
+		"username": fmt.Sprintf("__protectedFieldsPrivileged_user%v", createRandomInt()),
+		"password": "abcd1234.",
+	}
+
+	// Add the privilege "__protectedFieldsPrivileged" to the user3
+	plainUserWithPrivileges := westack.UserWithRoles{
+		Username: plainUser3.GetString("username"),
+		Password: plainUser3.GetString("password"),
+		Roles:    []string{"__protectedFieldsPrivileged"},
+	}
+	userWithPrivileges, err := westack.UpsertUserWithRoles(app, plainUserWithPrivileges, systemContext)
+	assert.NoError(t, err)
+	assert.NotNil(t, userWithPrivileges.GetID())
+	assert.NotEmptyf(t, userWithPrivileges.GetString("id"), "User should have an id")
+
+	// Login the user1
+	username1 := plainUser1.GetString("username")
+	password1 := plainUser1.GetString("password")
+	user1Token, err := loginUser(username1, password1, t)
+	assert.NoError(t, err)
+
+	// Login the user2
+	user2Token, err := loginUser(plainUser2.GetString("username"), plainUser2.GetString("password"), t)
+	assert.NoError(t, err)
+
+	// Login with admin
+	adminUsername := os.Getenv("WST_ADMIN_USERNAME")
+	adminPwd := os.Getenv("WST_ADMIN_PWD")
+	adminToken, err := loginUser(
+		adminUsername,
+		adminPwd,
+		t,
+	)
+	assert.NoError(t, err)
+
+	// Login with userWithPrivileges
+	userWithPrivilegesToken, err := loginUser(
+		plainUserWithPrivileges.Username,
+		plainUserWithPrivileges.Password,
+		t,
+	)
+	assert.NoError(t, err)
+	privilegedUserBearer := userWithPrivilegesToken.GetString("id")
+	// Extract the payload from the bearer
+	privilegedUserPayload := extractJWTPayload(t, privilegedUserBearer)
+	assert.Contains(t, privilegedUserPayload.Roles, "__protectedFieldsPrivileged")
+
+	// Get the user 1 through API with the user2
+	// Phone should not be returned with user2
+	user1RetrievedWithUser2, err := invokeApiJsonM(
+		t,
+		"GET",
+		fmt.Sprintf("/public-users/%v", user1.GetString("id")),
+		nil,
+		wst.M{
+			"Authorization": fmt.Sprintf("Bearer %v", user2Token["id"]),
+		},
+	)
+	assert.NoError(t, err)
+	assert.Equal(t, user1.GetString("id"), user1RetrievedWithUser2.GetString("id"))
+	assert.Equal(t, user1.GetString("username"), user1RetrievedWithUser2.GetString("username"))
+	assert.NotContainsf(t, user1RetrievedWithUser2, "password", "Password should not be returned")
+	assert.NotContainsf(t, user1RetrievedWithUser2, "phone", "Phone should not be returned")
+
+	// Get the user 1 through API with the user2
+	// Phone should not be returned with user2
+	encodedFilter, err := json.Marshal(wst.M{
+		"where": wst.M{
+			"_id": user1.GetString("id"),
+		},
+	})
+	users1RetrievedWithUser2, err := invokeApiJsonA(
+		t,
+		"GET",
+		fmt.Sprintf("/public-users?filter=%v", string(encodedFilter)),
+		nil,
+		wst.M{
+			"Authorization": fmt.Sprintf("Bearer %v", user2Token["id"]),
+		},
+	)
+	assert.NoError(t, err)
+	assert.EqualValues(t, 1, len(users1RetrievedWithUser2))
+	user1RetrievedWithUser2 = users1RetrievedWithUser2[0]
+
+	assert.Equal(t, user1.GetString("id"), user1RetrievedWithUser2.GetString("id"))
+	assert.Equal(t, user1.GetString("username"), user1RetrievedWithUser2.GetString("username"))
+	assert.NotContainsf(t, user1RetrievedWithUser2, "password", "Password should not be returned")
+	assert.NotContainsf(t, user1RetrievedWithUser2, "phone", "Phone should not be returned")
+
+	// Now get the user 1 through API with the admin token
+	// Phone should be returned with admin
+	user1RetrievedWithAdmin, err := invokeApiJsonM(
+		t,
+		"GET",
+		fmt.Sprintf("/public-users/%v", user1.GetString("id")),
+		nil,
+		wst.M{
+			"Authorization": fmt.Sprintf("Bearer %v", adminToken["id"]),
+		},
+	)
+	assert.NoError(t, err)
+	assert.Equal(t, user1.GetString("id"), user1RetrievedWithAdmin.GetString("id"))
+	assert.Equal(t, user1.GetString("username"), user1RetrievedWithAdmin.GetString("username"))
+	assert.NotContainsf(t, user1RetrievedWithAdmin, "password", "Password should not be returned")
+	assert.Containsf(t, user1RetrievedWithAdmin, "phone", "Phone should be returned")
+
+	// Phone should be returned also with user1 because it is the $owner
+	user1RetrievedWithUser1, err := invokeApiJsonM(
+		t,
+		"GET",
+		fmt.Sprintf("/public-users/%v", user1.GetString("id")),
+		nil,
+		wst.M{
+			"Authorization": fmt.Sprintf("Bearer %v", user1Token["id"]),
+		},
+	)
+	assert.NoError(t, err)
+	assert.Equal(t, user1.GetString("id"), user1RetrievedWithUser1.GetString("id"))
+	assert.Equal(t, user1.GetString("username"), user1RetrievedWithUser1.GetString("username"))
+	assert.NotContainsf(t, user1RetrievedWithUser1, "password", "Password should not be returned")
+	assert.Containsf(t, user1RetrievedWithUser1, "phone", "Phone should be returned")
+	assert.Equalf(t, user1.GetString("phone"), user1RetrievedWithUser1.GetString("phone"), "Phone should be the same")
+
+	// And Phone should be returned also with userWithPrivileges
+	user1RetrievedWithUserWithPrivileges, err := invokeApiJsonM(
+		t,
+		"GET",
+		fmt.Sprintf("/public-users/%v", user1.GetString("id")),
+		nil,
+		wst.M{
+			"Authorization": fmt.Sprintf("Bearer %v", userWithPrivilegesToken["id"]),
+		},
+	)
+	assert.NoError(t, err)
+	assert.Equal(t, user1.GetString("id"), user1RetrievedWithUserWithPrivileges.GetString("id"))
+	assert.Equal(t, user1.GetString("username"), user1RetrievedWithUserWithPrivileges.GetString("username"))
+	assert.NotContainsf(t, user1RetrievedWithUserWithPrivileges, "password", "Password should not be returned")
+	assert.Containsf(t, user1RetrievedWithUserWithPrivileges, "phone", "Phone should be returned")
+	assert.Equalf(t, user1.GetString("phone"), user1RetrievedWithUserWithPrivileges.GetString("phone"), "Phone should be the same")
+
+}
