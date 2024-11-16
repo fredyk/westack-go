9967998bdb061d63cd6eac33462df1acbe9a6175
fredyk <westack.team@gmail.com>
2024-11-16 14:58:04 +0100

    ADD: westack-go diagnose [permissions|launcher]

--- v2/cli-utils/clifuncs.go
+++ v2/cli-utils/clifuncs.go
@@ -7,5 +7,6 @@ func printHelp() {
 	log.Println("\tinit <target> \tInitializes a new project in the <target> directory")
 	log.Println("\tmodel add <model name> <datasource> \tCreates a new model with the given <model name> and attaches it to <datasource>")
 	log.Println("\tserver start \tStarts the server")
+	log.Println("\tdiagnose [permissions|launcher] \tRuns a diagnostic check on the server for debugging purposes")
 	log.Println()
 }
