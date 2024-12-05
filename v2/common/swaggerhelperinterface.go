01902ecbfa5de75fa48c09c182e1fa31d64199c5
fredyk <westack.team@gmail.com>
2024-12-05 13:42:40 +0100

    ADD: Open API Properties in models

--- v2/common/swaggerhelperinterface.go
+++ v2/common/swaggerhelperinterface.go
@@ -6,7 +6,7 @@ type SwaggerHelper interface {
 	// CreateOpenAPI creates a new OpenAPI specification and saves it to disk, or returns an error if it fails
 	CreateOpenAPI() error
 	// AddPathSpec adds a path specification to the OpenAPI specification
-	AddPathSpec(path string, verb string, verbSpec M)
+	AddPathSpec(path string, verb string, verbSpec M, operationName string, modelName string)
 	// Dump dumps the OpenAPI specification to disk, or returns an error if it fails
 	Dump() error
 }
