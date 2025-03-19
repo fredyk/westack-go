276e3939ec30e9938e4af7725197a3dfac4cad8e
Jhon Magdalena Vila <6827508+fredyk@users.noreply.github.com>
2025-03-19 20:09:01 +0000

    ADD: log user info

--- v2/westack/oauth.go
+++ v2/westack/oauth.go
@@ -312,6 +312,7 @@ func mountOauthRoutes(app *WeStack, loadedModel *model.StatefulModel, systemCont
 			}
 
 			var additionalUserInfo wst.M
+			fmt.Printf("[DEBUG] User Info: %v\n", userInfoData)
 			if additionalUserInfoMapping != nil {
 				additionalUserInfo = wst.M{}
 				for key, value := range additionalUserInfoMapping {
@@ -322,6 +323,7 @@ func mountOauthRoutes(app *WeStack, loadedModel *model.StatefulModel, systemCont
 					}
 				}
 			}
+			fmt.Printf("[DEBUG] Mapped user Info: %v\n", additionalUserInfo)
 
 			// check if userCredentials exists
 			userCredentials, err := app.accountCredentialsModel.FindOne(&wst.Filter{
