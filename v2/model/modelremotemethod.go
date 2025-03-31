ccac06ce0699e4f7417a2201d85a8d54633749c3
Jhon Magdalena Vila <6827508+fredyk@users.noreply.github.com>
2025-03-31 18:12:42 +0200

    REMOVE: comment out Content-Type setting in HandleRemoteMethod function

--- v2/model/modelremotemethod.go
+++ v2/model/modelremotemethod.go
@@ -497,7 +497,7 @@ func (loadedModel *StatefulModel) HandleRemoteMethod(name string, eventContext *
 
 					// io.Reader
 					if r, ok := eventContext.Result.(io.Reader); ok {
-						eventContext.Ctx.Set("Content-Type", "application/octet-stream")
+						// eventContext.Ctx.Set("Content-Type", "application/octet-stream")
 						fmt.Printf("Sending stream %T\n", r)
 						return eventContext.Ctx.Status(eventContext.StatusCode).SendStream(r, -1)
 					}
