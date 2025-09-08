3476f93a14b478dbf3d28d403c6686ba190d5054
Jhon Magdalena Vila <6827508+fredyk@users.noreply.github.com>
2025-09-08 12:00:01 +0200

    feat: add excludeFields filter option and make fields/excludeFields mutually exclusive

--- v2/common/common.go
+++ v2/common/common.go
@@ -660,13 +660,14 @@ type Order []string
 type Fields []string
 
 type Filter struct {
-	Where       *Where             `json:"where"`
-	Include     *Include           `json:"include"`
-	Order       *Order             `json:"order"`
-	Fields      *Fields            `json:"fields"`
-	Skip        int64              `json:"skip"`
-	Limit       int64              `json:"limit"`
-	Aggregation []AggregationStage `json:"aggregation"`
+	Where         *Where             `json:"where"`
+	Include       *Include           `json:"include"`
+	Order         *Order             `json:"order"`
+	Fields        *Fields            `json:"fields,omitempty"`
+	ExcludeFields *Fields            `json:"excludeFields,omitempty"`
+	Skip          int64              `json:"skip"`
+	Limit         int64              `json:"limit"`
+	Aggregation   []AggregationStage `json:"aggregation"`
 }
 
 // DeleteResult is the result of a DeleteMany operation.
